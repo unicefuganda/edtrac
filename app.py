7fa3a7d490902132a69f7deb1b86d15d2d5b6834
David McCann <david.a.mccann@gmail.com>
2010-10-27 11:43:50 +0300

    minor fix

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ class App (AppBase):
         # see if this contact matches any of our polls
         if (message.connection.contact):
             try:
-                poll = Poll.objects.filter(contacts=message.connection.contact).exclude(start_date=None).filter(Q(end_date=None) | (~Q(end_date=None) & Q(end_date__lt=datetime.datetime.now()))).latest('start_date')
+                poll = Poll.objects.filter(contacts=message.connection.contact).exclude(start_date=None).filter(Q(end_date=None) | (~Q(end_date=None) & Q(end_date__gt=datetime.datetime.now()))).latest('start_date')
                 response = poll.process_response(message)    
                 message.respond(response)
                 return True
