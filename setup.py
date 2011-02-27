d672784cc1577cd8bb995bdaee55301db30705f0
David McCann <david.a.mccann@gmail.com>
2011-02-27 15:40:56 +0300

    version modification

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     license="BSD",
 
-    requires = ["rapidsms (>=0.9.6a)"],
+    requires = ["rapidsms"],
 
     description='An extension for associating contacts with django users and groups.',
     long_description=open('README.rst').read(),
