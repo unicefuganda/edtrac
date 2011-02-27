3ea72ec2973de4adbfa4119bcfdad35de15b552e
David McCann <david.a.mccann@gmail.com>
2011-02-27 14:36:08 +0300

    package restructure, added setup

---
+++ setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+setup(
+    name='rapidsms-contact',
+    version='0.1',
+    license="BSD",
+
+    requires = ["rapidsms (>=0.9.6a)", 'rapidsms-generic'],
+
+    dependency_links = [
+        "http://github.com/daveycrockett/rapidsms-generic/tarball/master#egg=rapidsms-generic"
+    ],
+
+    description='Views for generic contact management.',
+    long_description=open('README.rst').read(),
+    author='Mugisha Moses',
+    author_email='mossplix@gmail.com',
+
+    url='http://github.com:/mossplix/rapidsms-contact',
+    download_url='http://github.com:/mossplix/rapidsms-contact/downloads',
+
+    include_package_data=True,
+
+    packages=['contact'],
+
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
