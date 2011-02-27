1104ee30f52f7976d753f479428645e6dc6f6ad8
David McCann <david.a.mccann@gmail.com>
2011-02-27 15:43:01 +0300

    version change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     license="BSD",
 
-    requires = ["rapidsms (>=0.9.6a)", 'rapidsms-generic'],
+    requires = ["rapidsms", 'rapidsms-generic'],
 
     dependency_links = [
         "http://github.com/daveycrockett/rapidsms-generic/tarball/master#egg=rapidsms-generic"
