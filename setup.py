23eded6f88e93f193d710017ed67a0c7096155d0
David McCann <david.a.mccann@gmail.com>
2011-02-27 14:32:05 +0300

    package restructuring

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+setup(
+    name='rapidsms-auth',
+    version='0.1',
+    license="BSD",
+
+    requires = ["rapidsms (>=0.9.6a)"],
+
+    description='An extension for associating contacts with django users and groups.',
+    long_description=open('README.rst').read(),
+    author='David McCann',
+    author_email='david.a.mccann@gmail.com',
+
+    url='http://github.com/daveycrockett/auth',
+    download_url='http://github.com/daveycrockett/auth/downloads',
+
+    include_package_data=True,
+
+    packages=['auth'],
+
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
