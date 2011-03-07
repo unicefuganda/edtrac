132c107c3a6c63c4ad2608d6c7d2a89f9f3f698c
David McCann <david.a.mccann@gmail.com>
2011-03-07 16:35:59 +0300

    changed permission name

--- unregister/forms.py
+++ unregister/forms.py
@@ -6,7 +6,7 @@ class BlacklistForm(ActionForm):
     """ abstract class for all the filter forms"""
     action_label = 'Blacklist/Opt-out Users'
     def perform(self, request, results):
-        if request.user and request.user.has_perm('blacklist_add_blacklist'):
+        if request.user and request.user.has_perm('unregister_add_blacklist'):
             connections = Connection.objects.filter(contact__in=results)
             for c in connections:
                 Blacklist.objects.get_or_create(connection=c)
