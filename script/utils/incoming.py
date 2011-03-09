9827a8a94b1191f0dd5fe3db27ea68e7de2eb436
Assey Alfred <asseym@gmail.com>
2011-03-09 17:29:54 +0300

    first steps of handling incoming responses to a poll in P (In progress) status

--- script/utils/incoming.py
+++ script/utils/incoming.py
@@ -1,4 +1,6 @@
 import datetime
+from script.models import ScriptProgress, ScriptStep
+from poll.models import Poll
 
 def incoming_progress(message):
     """
@@ -18,4 +20,38 @@ def incoming_progress(message):
     Returns: any immediate response (as a string) that is necessary (based on
     the rules of the script), or None if none are needed.
     """
-    pass
+    progress = ScriptProgress.objects.get(connection=message.connection)
+    num_script_steps = ScriptStep.objects.get(script=progress.script).count()
+    script_last_step = ScriptStep.objects.get(script=progress.script).order_by('-order')[0]
+    next_step = ScriptStep.objects.get(script=progress.script,order=progress.step.order+1)
+    current_time=datetime.datetime.now()
+    if progress.status == 'P':
+        response = Poll.process_response(message)
+        if response[0].has_errors:
+            if progress.step.rule == 'l':
+                if script_last_step == num_script_steps:
+                    progress.status = 'C'
+                    return None
+                else:
+                    progress.step = next_step
+                    progress.num_tries = progress.num_tries + 1
+                    progress.save()
+                    return None
+            elif progress.step.rule == 'R' or progress.step.rule == 'r':
+                step = ScriptStep.objects.get(script=progress.script)
+                current_poll_question = step.poll.question
+                if progress.num_tries < step.retry_offset:
+                    progress.num_tries = progress.num_tries + 1
+                    if progress.step.rule == 'r':
+                        progress.status = 'C'
+                    return current_poll_question
+                else:
+                    progress.step = next_step
+                    return None
+            else:
+                pass
+        else:
+            return response[1]
+    else:
+        response = Poll.process_response(message)
+        return response[1]
