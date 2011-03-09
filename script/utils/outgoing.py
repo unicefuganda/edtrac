b42725f9d333b68aaaa6f6b61bd264e078a28bb0
Mugisha Moses <moses@appfrica.org>
2011-03-09 11:31:09 +0300

    outgoing iteration 0

--- script/utils/outgoing.py
+++ script/utils/outgoing.py
@@ -17,4 +17,35 @@ def check_progress(connection):
     queued (based on the rules of the script), on None if none are 
     needed.
     """
-    pass
+    progress=ScriptProgress.objects.get(connection=connection)
+    number_of_script_steps=progress.script.steps.order_by('-order')[0].order
+    current_time=datetime.datetime.now()
+    if not progress.step:
+        progress.status='P'
+        progress.save()
+        progress.step=progress.script.steps.get(order=0)
+        progress.save()
+        return progress.step.message
+
+
+    elif progress.step.order==0 and progress.status:
+        progress.step=ScriptStep.objects.get(script=progress.script,order=1)
+        progress.step.save()
+        progress.status='P'
+        progress.save()
+        if progress.step.poll:
+            return progress.step.poll.question
+
+    elif current_time >= progress.time:
+
+        if progress.step.order == number_of_script_steps:
+            progress.status='C'
+            progress.save()
+            return None
+        progress.step=ScriptStep.objects.get(script=progress.script,order=progress.step.order+1)
+        progress.step.save()
+        progress.save()
+        if progress.step.poll:
+            return progress.step.poll.question
+
+    return progress.step.poll.question
