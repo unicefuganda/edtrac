97f1ec45257cd874cf72e775a5b6d3160807d485
David McCann <david.a.mccann@gmail.com>
2011-03-09 15:43:57 +0300

    minor fix to attribute creation for polls

--- poll/utils.py
+++ poll/utils.py
@@ -11,7 +11,7 @@ def init_attributes(sender, **kwargs):
 
     if site_table_created:
         if settings.SITE_ID and Site.objects.filter(pk=settings.SITE_ID).count():
-            Attribute.objects.get_or_create(slug="poll_number_value", 
+            Attribute.on_site.get_or_create(slug="poll_number_value",
                 defaults={
                     "slug": "poll_number_value",
                     "name": "Number",
@@ -19,21 +19,21 @@ def init_attributes(sender, **kwargs):
                     "datatype": "float",
                 }
             )
-            Attribute.objects.get_or_create(slug='poll_text_value',
+            Attribute.on_site.get_or_create(slug='poll_text_value',
                 defaults={
                     "name": "Text",
                     "description": "A response value for a Poll with expected text responses",
                     "datatype": "text",              
                 }
             )
-            Attribute.objects.get_or_create(slug='poll_location_value',
+            Attribute.on_site.get_or_create(slug='poll_location_value',
                 defaults={
                     "name": "Location",
                     "description": "A response value for a Poll with expected location-based responses",
                     "datatype": "object",
                 }
             )
-            Attribute.objects.get_or_create(slug='contact_location',
+            Attribute.on_site.get_or_create(slug='contact_location',
                 defaults={
                     "name": "Location",
                     "description": "The location associated with a particular contact",
