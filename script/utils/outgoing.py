44469bdf15a6ee924981ccd096ea6133ea4ccb3b
Mugisha Moses <moses@appfrica.org>
2011-03-11 13:02:52 +0300

    code refactoring

--- script/utils/outgoing.py
+++ script/utils/outgoing.py
@@ -22,40 +22,49 @@ def check_progress(connection):
     progress=ScriptProgress.objects.get(connection=connection)
     number_of_script_steps=progress.script.steps.order_by('-order')[0].order
     current_time=datetime.datetime.now()
+    #means its the first step in the script . get the initial step and check if its due to be sent.
     if not progress.step:
-        progress.step=progress.get_initial_step()
-        progress.status='P'
-        progress.save()
-    elif progress.step.order == number_of_script_steps and progress.step=='C':
-
-        return None
-    elif  current_time >= progress.time+datetime.timedelta(seconds=progress.step.giveup_offset):
-        progress.status='C'
-        progress.save()
-        return None
-    elif progress.num_tries==progress.step.num_tries :
-        if progress.step.rule in ['r','g']:
-            progress.status='C'
-            progress.save()
-            return None
-        elif progress.step.rule in ['R','w']:
-            progress.step=progress.get_next_step()
+        if current_time>=datetime.timedelta(seconds=progress.get_initial_step().start_offset)+progress.time:
+            progress.step=progress.get_initial_step()
             progress.status='P'
             progress.save()
-
-    elif progress.step==progress.get_initial_step() and progress.status=='C':
-        progress.step=progress.get_next_step()
-        progress.status='P'
-        progress.save()
-
-
-    elif current_time >= progress.time+datetime.timedelta(seconds=progress.step.start_offset):
-        progress.step=progress.get_next_step()
-        progress.step.save()
-        progress.save()
-
     else:
-        return None
+        ##check for completed step
+        if progress.step=='C':
+            #is this the last step?
+            if progress.step.order == number_of_script_steps:
+                return None
+            #get the next step and check its start_offset offset
+            elif current_time>=datetime.timedelta(seconds=progress.get_next_step().start_offset)+progress.time:
+                progress.step=progress.get_next_step()
+                progress.status='P'
+                progress.save()
+        #current progress is in progress
+        else:
+            #check giveup offset
+            if current_time >= progress.time+datetime.timedelta(seconds=progress.step.giveup_offset):
+                progress.status='C'
+                progress.save()
+                return None
+            #check number of tries
+            elif progress.num_tries==progress.step.num_tries :
+                if progress.step.rule in ['r','g']:
+                    progress.status='C'
+                    progress.save()
+                    return None
+                elif progress.step.rule in ['R','w']:
+                    progress.step=progress.get_next_step()
+                    progress.status='P'
+                    progress.save()
+
+
+            elif current_time >= progress.time+datetime.timedelta(seconds=progress.step.start_offset):
+                progress.step=progress.get_next_step()
+                progress.step.save()
+                progress.save()
+
+            else:
+                return None
     if progress.step.poll:
 
         return progress.step.poll.question
