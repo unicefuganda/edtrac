ccebb4a3dd9eadd55d161ae0ef5a5fc984958b40
David McCann <david.a.mccann@gmail.com>
2011-03-16 16:08:33 +0300

    added mass messages view

--- contact/urls.py
+++ contact/urls.py
@@ -3,11 +3,12 @@ from .views import add_contact, new_contact
 from .forms import FreeSearchForm, FilterGroupsForm, MassTextForm
 from rapidsms.models import Contact
 from generic.views import generic
-from .utils import get_messages
+from .utils import get_messages, get_mass_messages
 from django.contrib.auth.decorators import login_required
 from rapidsms_httprouter.models import Message
-from generic.sorters import SimpleSorter
+from generic.sorters import SimpleSorter, TupleSorter
 from .forms import FreeSearchTextForm, DistictFilterMessageForm, HandledByForm, ReplyTextForm
+from ureport.models import MassText
 
 urlpatterns = patterns('',
    url(r'^contact/index/$', generic, {'model':Contact, 'filter_forms':[FreeSearchForm, FilterGroupsForm], 'action_forms':[MassTextForm],'objects_per_page':25}),
@@ -29,5 +30,21 @@ urlpatterns = patterns('',
                  ],
       'sort_column':'date',
       'sort_ascending':False,
+    }, name="contact-messagelog"),
+   url(r'^contact/massmessages/$', login_required(generic), {
+      'model':MassText,
+      'queryset':get_mass_messages,
+      'objects_per_page':10,
+      'partial_row':'contact/partials/mass_message_row.html',
+      'base_template':'contact/mass_messages_base.html',
+      'columns':[('Message', True, 'text', TupleSorter(0)),
+                 ('Time', True, 'date', TupleSorter(1),),
+                 ('User', True, 'user', TupleSorter(2),),
+                 ('Recipients', True, 'response', TupleSorter(3),),
+                 ('Type', True, 'type', TupleSorter(4),),
+                 ],
+      'sort_column':'date',
+      'sort_ascending':False,
+      'selectable':False,
     }, name="contact-messagelog"),
 )
--- contact/utils.py
+++ contact/utils.py
@@ -1,4 +1,10 @@
 from rapidsms_httprouter.models import Message
+from ureport.models import MassText
+from poll.models import Poll
 
 def get_messages():
     return Message.objects.filter(direction='I')
+
+def get_mass_messages():
+    return [(p.question, p.start_date, p.user.username, p.contacts.count(), 'Poll Message') for p in Poll.objects.exclude(start_date=None)] + [(m.text, m.date, m.user.username, m.contacts.count(), 'Mass Text') for m in MassText.objects.all()]
+
