fbba429fa861ddf35e991673e4307a3193434952
David McCann <david.a.mccann@gmail.com>
2011-03-16 12:24:37 +0300

    minor fix to handled_by field for polls

--- poll/app.py
+++ poll/app.py
@@ -13,6 +13,13 @@ class App (AppBase):
             try:
                 poll = Poll.objects.filter(contacts=message.connection.contact).exclude(start_date=None).filter(Q(end_date=None) | (~Q(end_date=None) & Q(end_date__gt=datetime.datetime.now()))).latest('start_date')
                 response_obj, response_msg = poll.process_response(message)
+                if hasattr(message, 'db_message'):
+                    # if no other app handles this message, we want
+                    # the handled_by field set appropriately,
+                    # it won't since this app returns false
+                    db_message = message.db_message
+                    db_message.handled_by = 'poll'
+                    db_message.save()
                 message.respond(response_msg)
                 # play nice, let other things handle responses
                 return False
