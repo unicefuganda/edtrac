bd412b5cfa4d86d5d39b5ee9c97c41ac32428367
David McCann <david.a.mccann@gmail.com>
2011-03-19 20:00:21 +0300

    minor cleanup

--- script/utils/outgoing.py
+++ script/utils/outgoing.py
@@ -48,15 +48,15 @@ def check_progress(connection):
         else:
             progress.status = ScriptProgress.COMPLETE
             progress.save()
-            # shortcircuit transitions in this case, if we can move on
-            # move immediately
-#            if progress.time_to_transition(datetime.datetime.now()) and progress.moveon():
-#                return progress.outgoing_message()
+
     elif progress.time_to_resend(d_now):
         progress.num_tries = (progress.num_tries or 0) + 1
         progress.save()
         return progress.outgoing_message()
 
+    # This happens unconditionally, to shortcircuit the case
+    # where an expired step, set to COMPLETE above,
+    # can immidately transition to the next step
     d_now = datetime.datetime.now()
     if progress.time_to_transition(d_now) and progress.moveon():
             return progress.outgoing_message()
