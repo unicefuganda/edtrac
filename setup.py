2f4a7c1ad047cda0c999591bc40b35dd61be6534
David McCann <david.a.mccann@gmail.com>
2011-04-29 15:17:42 +0300

    setup fix

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     include_package_data=True,
 
     packages=['generic'],
-
+    package_data={packages[0]:['templates/*/*.html','templates/*/*/*.html']},
     zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
