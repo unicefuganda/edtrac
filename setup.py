f9a772c1b559dfdbe475fa3a3d9280e9b786048d
David McCann <david.a.mccann@gmail.com>
2011-05-02 17:19:54 +0300

    setup fix

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     include_package_data=True,
 
     packages=['contact'],
-    package_data={'generic':['templates/*/*.html','templates/*/*/*.html','static/*','static/*/*']},
+    package_data={'contact':['templates/*/*.html','templates/*/*/*.html','static/*','static/*/*']},
     zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
