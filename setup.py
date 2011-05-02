a81e3538a0f338971e5d4a07c01957c4762652c1
David McCann <david.a.mccann@gmail.com>
2011-05-02 14:31:15 +0300

    setup fix

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     include_package_data=True,
 
     packages=['auth'],
-    package_data={'auth':['templates/*/*.html','templates/*/*/*.html']},
+        package_data={'generic':['templates/*/*.html','templates/*/*/*.html','static/*','static/*/*']},
     zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
