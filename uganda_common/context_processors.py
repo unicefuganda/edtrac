1a9b314b9663987086d774477310e2c77ee0f01d
David McCann <david.a.mccann@gmail.com>
2011-05-04 08:38:29 +0300

    moved context processors for rapid

---
+++ uganda_common/context_processors.py
@@ -0,0 +1,15 @@
+"""A set of request processors that return dictionaries to be merged into a
+template context. Each function takes the request object as its only parameter
+and returns a dictionary to add to the context.
+"""
+from django.conf import settings
+def layout(request):
+    """
+    a context processor that changes the base css of the layout.html in RapidSMS. This is useful in case you want to
+    have a custom skin for RapidSMS.
+    """
+    css = settings.BASE_CSS if settings.BASE_CSS else "/static/rapidsms/stylesheets/layout.css"
+    return {
+        "BASE_CSS":settings.BASE_CSS
+    }
+
