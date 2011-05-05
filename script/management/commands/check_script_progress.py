d3c3ce47100de2e8222747dc4e7685ac3956e203
David McCann <david.a.mccann@gmail.com>
2011-05-05 13:00:22 +0300

    added early and late parameters to check_script_progress to allow 'nice' sending of automated messages

--- script/management/commands/check_script_progress.py
+++ script/management/commands/check_script_progress.py
@@ -13,22 +13,28 @@ from rapidsms.messages.outgoing import OutgoingMessage
 
 from script.utils.outgoing import check_progress
 from script.models import ScriptProgress
+from optparse import OptionParser, make_option
+import datetime
 
 class Command(BaseCommand):
 
+    option_list = BaseCommand.option_list + (
+        make_option("-e", "--early", dest="e"),
+        make_option("-l", "--late", dest="l")
+    )
+
     @transaction.commit_manually
     def handle(self, **options):
-        try:
-            router = get_router()
-            for connection in ScriptProgress.objects.values_list('connection', flat=True).distinct():
-                connection=Connection.objects.get(pk=connection)
-                response = check_progress(connection)
-                print response
-                if response:
-                    router.add_outgoing(connection, response)
-                transaction.commit()
-        except Exception, exc:
-            transaction.rollback()
-            print traceback.format_exc(exc)
-
-
+        if datetime.datetime.now().hour in range(int(options['e']),int(options['l'])):
+            try:
+                router = get_router()
+                for connection in ScriptProgress.objects.values_list('connection', flat=True).distinct():
+                    connection=Connection.objects.get(pk=connection)
+                    response = check_progress(connection)
+                    print response
+                    if response:
+                        router.add_outgoing(connection, response)
+                    transaction.commit()
+            except Exception, exc:
+                transaction.rollback()
+                print traceback.format_exc(exc)
