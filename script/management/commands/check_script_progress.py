889442ef2df0556cfb6abe216fb264e5a9564061
Assey Alfred <asseym@gmail.com>
2011-06-16 13:22:01 +0300

    increased execution time for check_script_progress command to 1 and half minutes from 35 seconds to allow more connection to be processed in one batch

--- script/management/commands/check_script_progress.py
+++ script/management/commands/check_script_progress.py
@@ -50,38 +50,38 @@ class Command(BaseCommand):
         if recipients:
             recipients = [email for name,email in recipients]
         if current.hour in range(int(options['e']),int(options['l'])):
-                router = HttpRouter()
-                unstarted = ScriptProgress.objects.filter(step=None).values_list('connection', flat=True).distinct()
-                started = ScriptProgress.objects.order_by('step').values_list('connection', flat=True).distinct()
-                for connection in itertools.chain(unstarted, started):
-                    try:
-                        log_str=" PK:"+str(connection)
-                        connection=Connection.objects.get(pk=connection)
-                        script_p=ScriptProgress.objects.get(connection=connection)
-                        log_str=log_str+" Step Before: "+str(script_p.step)
-                        if script_p.step:
-                            log_str=log_str+" start offset: "+str(script_p.step.start_offset)
-                            
-                        response = check_progress(connection)
-                        log_str=log_str+" Step After: "+str(script_p.step)
-                        log_str=log_str+"Response: "+str(response)
-                        logger.info(log_str)
-                        if response:
-                            if type(response) == Email and connection.contact and connection.contact.user:
-                                response.recipients.clear()
-                                response.recipients.add(connection.contact.user)
-                                response.send(context={'connection':connection})
-                            else:
-                                template = Template(response)
-                                context = Context({'connection':connection})
-                                router.add_outgoing(connection, template.render(context))
-                        transaction.commit()
-                        if datetime.datetime.now() - current > datetime.timedelta(seconds=35):
-                            return
-                    except Exception, exc:
-                        transaction.rollback()
-                        print traceback.format_exc(exc)
-                        logger.debug(str(exc))
-                        if recipients:
-                            send_mail('[Django] Error: check_script_progress cron', str(traceback.format_exc(exc)), 'root@uganda.rapidsms.org', recipients, fail_silently=True)
-                        continue
+            router = HttpRouter()
+            unstarted = ScriptProgress.objects.filter(step=None).values_list('connection', flat=True).distinct()
+            started = ScriptProgress.objects.order_by('step').values_list('connection', flat=True).distinct()
+            for connection in itertools.chain(unstarted, started):
+                try:
+                    log_str=" PK:"+str(connection)
+                    connection=Connection.objects.get(pk=connection)
+                    script_p=ScriptProgress.objects.get(connection=connection)
+                    log_str=log_str+" Step Before: "+str(script_p.step)
+                    if script_p.step:
+                        log_str=log_str+" start offset: "+str(script_p.step.start_offset)
+                        
+                    response = check_progress(connection)
+                    log_str=log_str+" Step After: "+str(script_p.step)
+                    log_str=log_str+"Response: "+str(response)
+                    logger.info(log_str)
+                    if response:
+                        if type(response) == Email and connection.contact and connection.contact.user:
+                            response.recipients.clear()
+                            response.recipients.add(connection.contact.user)
+                            response.send(context={'connection':connection})
+                        else:
+                            template = Template(response)
+                            context = Context({'connection':connection})
+                            router.add_outgoing(connection, template.render(context))
+                    transaction.commit()
+                    if datetime.datetime.now() - current > datetime.timedelta(seconds=95):
+                        return
+                except Exception, exc:
+                    transaction.rollback()
+                    print traceback.format_exc(exc)
+                    logger.debug(str(exc))
+                    if recipients:
+                        send_mail('[Django] Error: check_script_progress cron', str(traceback.format_exc(exc)), 'root@uganda.rapidsms.org', recipients, fail_silently=True)
+                    continue
