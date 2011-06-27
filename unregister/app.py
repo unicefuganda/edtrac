4bb2a81a4fe52b62b70de61f966b58a230d5164b
David McCann <david.a.mccann@gmail.com>
2011-06-27 08:41:55 +0300

    auto opt-out

--- unregister/app.py
+++ unregister/app.py
@@ -2,11 +2,16 @@ import rapidsms
 
 from rapidsms.apps.base import AppBase
 from .models import Blacklist
+from django.conf import settings
 
 class App (AppBase):
 
     def handle (self, message):
         if Blacklist.objects.filter(connection=message.connection).count():
+            return True
+        elif message.text.strip() in settings.OPT_OUT_WORDS:
+            Blacklist.objects.create(connection=message.connection)
+            message.respond(settings.OPT_OUT_CONFIRMATION)
             return True
         return False
 
