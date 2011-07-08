7846be1ad8063eb38ef1fbc7510edba4c5ed1922
David McCann <david.a.mccann@gmail.com>
2011-07-08 10:46:19 +0300

    removed simple_locations depencency

--- setup.py
+++ setup.py
@@ -12,14 +12,12 @@ setup(
         'django-eav',
         'rapidsms-polls',
         'rapidsms-httprouter',
-        'simple-locations',
     ],
 
     dependency_links = [
         "http://github.com/mvpdev/django-eav/tarball/master#egg=django-eav",
         "http://github.com/daveycrockett/rapidsms-polls/tarball/master#egg=rapidsms-polls",
         "http://github.com/daveycrockett/rapidsms-httprouter/tarball/master#egg=rapidsms-httprouter",
-        "http://github.com/mossplix/simple_locations/tarball/master#egg=simple-locations",
         "http://github.com/daveycrockett/rapidsms-polls/tarball/master#egg=rapidsms-polls",
     ],
 
