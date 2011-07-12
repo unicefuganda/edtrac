f0328debadb5fed5a3e7ef9567eb9192dbf72381
Rowena <rluk@dimagi.com>
2011-07-12 12:28:05 +0300

    add an admin for location type

--- lib/rapidsms/contrib/locations/admin.py
+++ lib/rapidsms/contrib/locations/admin.py
@@ -2,9 +2,12 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
 from django.contrib import admin
-from .models import Location
+from .models import Location, LocationType
 
 class LocationAdmin(admin.ModelAdmin):
     model = Location
+class LocationTypeAdmin(admin.ModelAdmin):
+    model = LocationType
 
 admin.site.register(Location, LocationAdmin)
+admin.site.register(LocationType, LocationTypeAdmin)
