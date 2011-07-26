45792d45e193ee00253d21d1c8e9d7e92e005170
David McCann <david.a.mccann@gmail.com>
2011-07-26 12:24:54 +0300

    more cleanup of time slider JS functionality

--- generic/static/javascripts/slider.js
+++ generic/static/javascripts/slider.js
@@ -1,48 +1,71 @@
-function selected_layer(data){
-	$('#date_slider').append('<input type="hidden" class="layer_data" name="layer_data" value="'+data+'" />');
+var update_date_layers = null;
+
+function init_timeslider(min_ts) {
+    //initialise ui slider
+	$('select#start, select#end').selectToUISlider({
+        labels: 0,
+		sliderOptions: {
+		    change:on_timeslider_change
+        }
+	});
+
+	// Make tick marks at every Monday on the
+	// time slider UI
+	min_sdt = new Date(min_ts);
+    tickEvery(7, min_sdt.getDay() - 1);
+
+    // trigger the slider's label to update
+    $('select#end option').change();
+}
+
+
+function on_timeslider_change(event, element) {
+    start_date = new Date(parseInt($('select#start option').eq(element.values[0])[0].value));
+    end_date = new Date(parseInt($('select#end option').eq(element.values[1])[0].value));
+
+    start_str = start_date.getDate() + "-" + (start_date.getMonth() + 1) + "-" + start_date.getFullYear();
+    end_str = end_date.getDate() + "-" + (end_date.getMonth() + 1) + "-" + end_date.getFullYear();	
+	$( "#date_slider_value" ).text( "From " + start_str + " to " + end_str );
 }
 
 
 function tickEvery(num, offset){
     $('.ui-slider span.ui-slider-tic').each(function(index,elem){
-        if((index + 1) % (num + offset) == 0){
+        if (((index + offset) % num) == 0) {
             $(elem).css('display','block');
         }
     });
 }
 
 
-function previous_date_range(timespan){
-	var d = new Date();
-	var last_date = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 0, 0, 0, 0)
-	var last_timestamp = parseInt(last_date.getTime());
-	var last_date_index = $('select#end option').index($('select#end option[value='+last_timestamp+']'));
-	if (timespan == 'w') {
-		var offset = last_date.getDay();
-		var end_date = parseInt($('select#end option')[last_date_index-(1+offset)].value);
-		var start_date = parseInt($('select#end option')[last_date_index-(7+offset)].value);
-	} else {
-		var month = last_date.getMonth();
-		var year = last_date.getYear();
-		var current_month_date = 32 - new Date(year, month, 32).getDate();
-		var last_month_last_day = new Date((new Date(year, month,1))-1).getDate();
-		var end_date = parseInt($('select#end option')[$('select#end option').length-(1+current_month_date)].value);
-		var start_date = parseInt($('select#end option')[$('select#end option').length-(current_month_date+last_month_last_day)].value);
+var DAY_MS = 86400000;
+
+function previous_date_range(timespan) {
+	now = new Date();
+	base = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+	switch (timespan) {
+	case 'w':
+		end_date = base;
+		while (end_date.getDay() != 0) {
+			end_date = new Date(end_date.getTime() - DAY_MS);
+		}
+		start_date = new Date(end_date.getTime() - (7 * DAY_MS));
+		move_slider(start_date, end_date);
+		break;
+	case 'm':
+		end_date = new Date(base.getFullYear(), base.getMonth(), 1);
+		end_date = new Date(end_date.getTime() - DAY_MS);
+		start_date = new Date(end_date.getFullYear(), end_date.getMonth(), 1);
+		move_slider(start_date, end_date);
+		break;
 	}
-	
-    // update the hidden fields
-	$('#id_start_ts').val(start_date);
-	$('#id_end_ts').val(end_date);
-	
-	// move the slider UI itself
-	move_slider(start_date, end_date);
 }
 
 
-function move_slider(start, end) {
-    $('select#start option[value='+start+']').attr('selected','selected');
-    $('select#end option[value='+end+']').attr('selected','selected');
-    $('select#start option').change();
-    $('select#end option').change();
-    $('form#date_range_form').submit();
+function move_slider(start_date, end_date) {
+    $('select#start').val(start_date.getTime());
+    $('select#end').val(end_date.getTime());
+    $('select#start').change();
+    $('select#end').change();
+    update_date_layers();
 }
