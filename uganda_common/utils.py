69cefc0352600309c1aea49fdeee4b4d6e18318f
Assey Alfred <asseym@gmail.com>
2011-08-01 18:14:17 +0300

    added ability to match whole string to find_closest_match()

--- uganda_common/utils.py
+++ uganda_common/utils.py
@@ -173,21 +173,24 @@ def find_best_response(session, poll):
             return resp.eav.poll_location_value
     return None
 
-def find_closest_match(value, model):
+def find_closest_match(value, model, match_exact=False):
     string_template = STARTSWITH_PATTERN_TEMPLATE % '[a-zA-Z]*'
     regex = re.compile(string_template)
     try:
-        if regex.search(value):
-            spn = regex.search(value).span()
-            name_str = value[spn[0]:spn[1]]
-            toret = None
-            model_names = model.values_list('name', flat=True)
-            model_names_lower = [ai.lower() for ai in model_names]
-            model_names_matches = difflib.get_close_matches(name_str.lower(), model_names_lower)
-            print "model names lower = %s" % model_names_lower
-            if model_names_matches:
-                toret = model.get(name__iexact=model_names_matches[0])
-                return toret
+        if match_exact:
+            name_str = value
+        else:
+            if regex.search(value):
+                spn = regex.search(value).span()
+                name_str = value[spn[0]:spn[1]]
+        toret = None
+        model_names = model.values_list('name', flat=True)
+        model_names_lower = [ai.lower() for ai in model_names]
+        model_names_matches = difflib.get_close_matches(name_str.lower(), model_names_lower)
+        print "model names lower = %s" % model_names_lower
+        if model_names_matches:
+            toret = model.get(name__iexact=model_names_matches[0])
+            return toret
     except Exception, exc:
 #            print traceback.format_exc(exc)
             return None
