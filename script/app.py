57465498dace3c385fc190029d4fd595b4a0245d
David McCann <david.a.mccann@gmail.com>
2011-08-03 15:02:52 +0300

    bug fix

--- script/app.py
+++ script/app.py
@@ -9,7 +9,7 @@ class App (AppBase):
 
     def handle (self, message):
         try:
-            progress = ScriptProgress.objects.filter(connection=message.connection).order_by('-time')
+            progress = ScriptProgress.objects.filter(connection=message.connection).order_by('-time')[0]
             if progress.count():
                 progress = progress[0]
             script_last_step = ScriptStep.objects.filter(script=progress.script).order_by('-order')[0]
