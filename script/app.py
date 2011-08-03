fe5296b7830ed98b52ef58548895e0e60bd38b9a
David McCann <david.a.mccann@gmail.com>
2011-08-03 15:04:07 +0300

    whoops

--- script/app.py
+++ script/app.py
@@ -9,17 +9,17 @@ class App (AppBase):
 
     def handle (self, message):
         try:
-            progress = ScriptProgress.objects.filter(connection=message.connection).order_by('-time')[0]
+            progress = ScriptProgress.objects.filter(connection=message.connection).order_by('-time')
             if progress.count():
                 progress = progress[0]
-            script_last_step = ScriptStep.objects.filter(script=progress.script).order_by('-order')[0]
-            if progress.step and progress.step.order == script_last_step.order and progress.status == 'C':
-                return False
-            else:
-                response = incoming_progress(message)
-                if response:
-                    message.respond(response)
-                return True
+                script_last_step = ScriptStep.objects.filter(script=progress.script).order_by('-order')[0]
+                if progress.step and progress.step.order == script_last_step.order and progress.status == 'C':
+                    return False
+                else:
+                    response = incoming_progress(message)
+                    if response:
+                        message.respond(response)
+                    return True
         except ScriptProgress.DoesNotExist:
             pass
 
