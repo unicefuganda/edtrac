7b764146a6343a4d503c2a9012ec8d10c05ade18
Drew Roos <droos@dimagi.com>
2011-09-27 12:50:19 -0400

    fix import breakage

--- poll/extensions/rapidsms/contact.py
+++ poll/extensions/rapidsms/contact.py
@@ -1,14 +1,12 @@
 from django.db import models
 
-from rapidsms.contrib.locations.models import Location
-
 class LocatedContact(models.Model):
     """
     This extension for Contacts allows developers to tie a Contact to
     the Area object they're reporting from.  This extension
     depends on the simple_locations app.
     """
-    reporting_location = models.ForeignKey(Location, blank=True, null=True)
+    reporting_location = models.ForeignKey('locations.Location', blank=True, null=True)
 
     class Meta:
         abstract = True
