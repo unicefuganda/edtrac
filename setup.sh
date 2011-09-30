b9c032e019223b3b7ccc278a886c694c460c0ebd
Mugisha Moses <mossplix@gmail.com>
2011-09-30 12:24:27 +0300

    initial setup for the project

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+# script to setup the postgis database and setup the respective database tables
+
+# create the database (first drop the old one, if it exists)
+#
+dropdb geoserver
+createdb geoserver -T template_postgis
+
+#create a table to contain district name, slug,and poll info (yes,or no)
+psql -d geoserver -c "create table population (id char(5), district varchar(100), poll_id integer ,poll_result varchar(3));"
+
+# load the shapefile
+#
+ogr2ogr -f PostgreSQL PG:dbname=geoserver Uganda_Districts2010/Uganda_districts2010.shp -t_srs EPSG:900913 -nlt multipolygon -nln Uganda_districts2010
+
