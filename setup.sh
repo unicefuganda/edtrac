16791f94e8731a495a64351a3a11db213bf08adc
Mugisha Moses <mossplix@gmail.com>
2011-10-19 11:12:34 +0300

    change to  postgres user

--- setup.sh
+++ setup.sh
@@ -4,24 +4,24 @@
 
 #create a postgis spatial database template
 
-POSTGIS_SQL_PATH=/usr/share/postgresql/8.4/contrib/postgis-1.5
-createdb -U postgres -E UTF8 template_postgis # Create the template spatial database.
-createlang -U postgres -d template_postgis plpgsql # Adding PLPGSQL language support.
-psql -U postgres -d postgres -c "UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis';"
-psql -U postgres -d template_postgis -f $POSTGIS_SQL_PATH/postgis.sql # Loading the PostGIS SQL routines
-psql -U postgres -d template_postgis -f $POSTGIS_SQL_PATH/spatial_ref_sys.sql
-psql -U postgres -d template_postgis -c "GRANT ALL ON geometry_columns TO PUBLIC;" # Enabling users to alter spatial tables.
-psql -U postgres  -d template_postgis -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
+POSTGIS_SQL_PATH=/usr/share/postgresql/8.4/contrib
+createdb  -E UTF8 template_postgis3 # Create the template spatial database.
+createlang  -d template_postgis3 plpgsql # Adding PLPGSQL language support.
+sudo -u postgres psql  -d postgres -c "UPDATE pg_database SET datistemplate='true' WHERE datname='template_postgis3';"
+sudo -u postgres psql  -d template_postgis3 -f $POSTGIS_SQL_PATH/postgis.sql # Loading the PostGIS SQL routines
+sudo -u postgres psql  -d template_postgis3 -f $POSTGIS_SQL_PATH/spatial_ref_sys.sql
+sudo -u postgres psql  -d template_postgis3 -c "GRANT ALL ON geometry_columns TO PUBLIC;" # Enabling users to alter spatial tables.
+sudo -u postgres psql   -d template_postgis3 -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
 
 # create the database (first drop the old one, if it exists)
 #
-dropdb  -U postgres geoserver
-createdb  -U postgres geoserver -T template_postgis
+sudo -u postgres dropdb   geoserver
+sudo -u postgres createdb   geoserver -T template_postgis3
 
 #create a table to contain district name, slug,and poll info (yes,or no)
-psql -d geoserver -U postgres -c "create table district (id char(5), district varchar(100), slug varchar(100),iso_code varchar(5), poll_id integer ,poll_result varchar(3));"
+#sudo -u postgres psql -d geoserver  -c "create table district (id char(5), district varchar(100), slug varchar(100),iso_code varchar(5), poll_id integer ,poll_result varchar(3));"
 
 # load the shapefile
 #
-ogr2ogr -f  "PostgreSQL" -t_srs EPSG:900913 PG:"dbname=geoserver user=postgres" Uganda_District2010/Uganda_districts2010.shp  -nlt multipolygon -nln Uganda_districts2010
+sudo -u postgres ogr2ogr -f  "PostgreSQL" -t_srs EPSG:900913 PG:"dbname=geoserver" Uganda_District2010/Uganda_districts2010.shp  -nlt multipolygon -nln Uganda_districts2010
 
