ab8716ae63751ebcf329a16e050f571a3a5de614
David McCann <david.a.mccann@gmail.com>
2011-10-19 14:18:45 +0300

    added location extensions

--- geoserver/models.py
+++ geoserver/models.py
@@ -1,12 +1,11 @@
 from django.db import models
+from poll.models import Poll
+
 class PollData(models.Model):
-    district = models.CharField(max_length=50, blank=True,null=True)
-    yes=models.IntegerField(max_length=9, blank=True,null=True,default=0)
-    no=models.IntegerField(max_length=9, blank=True,null=True,default=0)
-    uncategorized=models.IntegerField(max_length=9, blank=True,null=True,default=0)
-    poll=models.IntegerField(max_length=5, blank=True,null=True)
-    unknown=models.IntegerField(max_length=5, blank=True,null=True,default=0)
-    dominant_category=models.CharField(max_length=10, blank=True)
+    district = models.CharField(max_length=100, blank=True, null=True)
+    yes = models.FloatField(blank=True, null=True, default=0)
+    no = models.FloatField(blank=True, null=True, default=0)
+    uncategorized = models.FloatField(blank=True, null=True, default=0)
+    unknown = models.FloatField(max_length=5, blank=True, null=True, default=0)
+    poll = models.ForeignKey(Poll, null=True)
 
-
-    
