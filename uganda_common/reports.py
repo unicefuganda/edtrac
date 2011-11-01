d380e1eaa596212e8358588e21466235dba8dfa2
David McCann <david.a.mccann@gmail.com>
2011-11-01 15:23:03 +0300

    updated to new CBV generic reports

--- uganda_common/reports.py
+++ uganda_common/reports.py
@@ -1,26 +1,181 @@
-from generic.reports import Column, Report
+from generic.reports import Report
+from generic.reporting.reports import Column
 from .utils import total_submissions, reorganize_location, total_attribute_value
 from rapidsms.contrib.locations.models import Location
+from generic.reporting.views import BasicDateGetter
+from generic.reporting.forms import DateRangeForm
+from django.db import connection
+import datetime
+
+class XFormDateGetter(BasicDateGetter):
+    def get_dates(self, request):
+        """
+        Process date variables from POST, session, or defaults
+        """
+        if request.POST:
+            form = DateRangeForm(request.POST)
+            if form.is_valid():
+                cursor = connection.cursor()
+                cursor.execute("select min(created) from rapidsms_xforms_xformsubmission")
+                min_date = cursor.fetchone()[0] or (datetime.datetime.now() - datetime.timedelta(365))
+                start_date = form.cleaned_data['start']
+                end_date = form.cleaned_data['end']
+                request.session['start_date'] = start_date
+                request.session['end_date'] = end_date
+                return {
+                    'max':datetime.datetime.now(),
+                    'min':min_date,
+                    'start':start_date,
+                    'end':end_date,
+                }
+            else:
+                return {}
+        else:
+            cursor = connection.cursor()
+            cursor.execute("select min(created), max(created) from rapidsms_xforms_xformsubmission")
+            min_date, end_date = cursor.fetchone()
+            end_date = end_date or datetime.datetime.now()
+            min_date = min_date or (datetime.datetime.now() - datetime.timedelta(365))
+            start_date = end_date - datetime.timedelta(days=30)
+            if request.session.get('start_date', None)  and request.session.get('end_date', None):
+                start_date = request.session['start_date']
+                end_date = request.session['end_date']
+
+            return {
+                'max':datetime.datetime.now(),
+                'min':min_date,
+                'start':start_date,
+                'end':end_date,
+            }
+
+
+class QuotientColumn(Column):
+    def __init__(self, top_column, bottom_column, **kwargs):
+        Column.__init__(self, **kwargs)
+        self.top_column = top_column
+        self.bottom_column = bottom_column
+        self.chart_yaxis = 'Percentage of Reports'
+        if not self.chart_title:
+            self.chart_title = 'Variation of %s' % self.get_title()
+
+
+    def set_report(self, report):
+        Column.set_report(self, report)
+        self.top_column.set_report(report)
+        self.bottom_column.set_report(report)
+
+
+    def add_to_report(self, report, key, dictionary):
+        temp = {}
+        self.top_column.add_to_report(report, 'top', temp)
+        self.bottom_column.add_to_report(report, 'bottom', temp)
+        for row_key, items in temp.items():
+            if 'bottom' in items:
+                bottom = items['bottom']
+                top = items.setdefault('top', 0)
+                quotient = round(((float(top) / bottom) * 100), 1)
+                dictionary.setdefault(row_key, {'location_name':items['location_name']})
+                dictionary[row_key][key] = quotient
+
+
+    def get_chart(self):
+        from .views import PercentageChartView
+        return PercentageChartView(location_id=self.report.location.pk, \
+                         start_date=self.report.start_date, \
+                         end_date=self.report.end_date, \
+                         top_column=self.top_column, \
+                         bottom_column=self.bottom_column,
+                         chart_title=self.chart_title,
+                         chart_subtitle=self.chart_subtitle,
+                         chart_yaxis=self.chart_yaxis)
+
+    def get_view_function(self):
+        from .views import PercentageChartView
+        return PercentageChartView.as_view(location_id=self.report.location.pk, \
+                         start_date=self.report.start_date, \
+                         end_date=self.report.end_date, \
+                         top_column=self.top_column, \
+                         bottom_column=self.bottom_column,
+                         chart_title=self.chart_title,
+                         chart_subtitle=self.chart_subtitle,
+                         chart_yaxis=self.chart_yaxis)
 
 
 class XFormSubmissionColumn(Column):
-    def __init__(self, keyword, extra_filters=None):
+    def __init__(self, keyword, extra_filters=None, **kwargs):
+        Column.__init__(self, **kwargs)
         self.keyword = keyword
         self.extra_filters = extra_filters
+        self.chart_yaxis = 'Number of Reports'
+        if not self.chart_title:
+            self.chart_title = 'Variation of %s' % self.get_title()
 
     def add_to_report(self, report, key, dictionary):
         val = total_submissions(self.keyword, report.start_date, report.end_date, report.location, self.extra_filters)
         reorganize_location(key, val, dictionary)
 
+    def get_chart(self):
+        from .views import XFormChartView
+        return XFormChartView(location_id=self.report.location.pk, \
+                         start_date=self.report.start_date, \
+                         end_date=self.report.end_date, \
+                         xform_keyword=self.keyword,
+                         extra_filters=self.extra_filters,
+                         chart_title=self.chart_title,
+                         chart_subtitle=self.chart_subtitle,
+                         chart_yaxis=self.chart_yaxis)
+
+    def get_view_function(self):
+        from .views import XFormChartView
+        return XFormChartView.as_view(location_id=self.report.location.pk, \
+                         start_date=self.report.start_date, \
+                         end_date=self.report.end_date, \
+                         xform_keyword=self.keyword,
+                         extra_filters=self.extra_filters,
+                         chart_title=self.chart_title,
+                         chart_subtitle=self.chart_subtitle,
+                         chart_yaxis=self.chart_yaxis)
+
 
 class XFormAttributeColumn(Column):
-    def __init__(self, keyword, extra_filters=None):
+    def __init__(self, keyword, extra_filters=None, **kwargs):
+        Column.__init__(self, **kwargs)
         self.keyword = keyword
+        s = self.keyword.split('_')
+        self.xform_keyword = s[0]
+        self.attribute_keyword = s[1]
         self.extra_filters = extra_filters
+        self.chart_yaxis = 'Number of Reports'
+        if not self.chart_title:
+            self.chart_title = 'Variation of %s' % self.get_title()
 
     def add_to_report(self, report, key, dictionary):
         val = total_attribute_value(self.keyword, report.start_date, report.end_date, report.location, self.extra_filters)
         reorganize_location(key, val, dictionary)
+
+    def get_chart(self):
+        from .views import XFormChartView
+        return XFormChartView(location_id=self.report.location.pk, \
+                         start_date=self.report.start_date, \
+                         end_date=self.report.end_date, \
+                         xform_keyword=self.xform_keyword,
+                         attribute_keyword=self.attribute_keyword,
+                         extra_filters=self.extra_filters,
+                         chart_title=self.chart_title,
+                         chart_subtitle=self.chart_subtitle,
+                         chart_yaxis=self.chart_yaxis)
+
+    def get_view_function(self):
+        from .views import XFormChartView
+        return XFormChartView.as_view(location_id=self.report.location.pk, \
+                         start_date=self.report.start_date, \
+                         end_date=self.report.end_date, \
+                         xform_keyword=self.xform_keyword,
+                         attribute_keyword=self.attribute_keyword,
+                         extra_filters=self.extra_filters,
+                         chart_title=self.chart_title,
+                         chart_subtitle=self.chart_subtitle,
+                         chart_yaxis=self.chart_yaxis)
 
 
 class PollNumericResultsColumn(Column):
---
+++ uganda_common/views.py
@@ -0,0 +1,181 @@
+import datetime, time
+from generic.reporting.views import ChartView, ReportView
+from django.shortcuts import get_object_or_404
+from rapidsms.contrib.locations.models import Location
+from uganda_common.utils import total_submissions as total_submissions_api, total_attribute_value as total_attribute_value_api
+from uganda_common.reports import XFormDateGetter
+
+class XFormReport(ReportView):
+    date_getter = XFormDateGetter()
+
+class XFormChartView(ChartView):
+    location_id = None
+    xform_keyword = None
+    attribute_keyword = None
+    extra_filters = None
+    date_getter = XFormDateGetter()
+
+    def as_url(self):
+        return ''
+
+    def reorganize_for_chart_api(self, timespan, report):
+        to_ret = []
+        if len(report):
+            series = []
+            cur_loc = report[0]['location_name']
+            current_series_obj = {'name':str(cur_loc), 'data':series}
+            to_ret.append(current_series_obj)
+            for d in report:
+                if d['location_name'] != cur_loc:
+                    series = []
+                    current_series_obj = {'name':str(d['location_name']), 'data':series}
+                    to_ret.append(current_series_obj)
+                    cur_loc = d['location_name']
+                dt = d[timespan]
+                if timespan == 'month':
+                    dt = datetime.datetime(int(d['year']), int(dt), 1)
+                elif timespan == 'week':
+                    dt = datetime.datetime(int(d['year']), 1, 1) + datetime.timedelta(days=(int(dt) * 7))
+                elif timespan == 'quarter':
+                    dt = datetime.datetime(int(d['year']), int(dt) * 3, 1)
+                ts = int(time.mktime(dt.timetuple()))
+                series.append([ts, d['value']])
+
+        return to_ret
+
+
+    def get_data(self):
+        location = get_object_or_404(Location, pk=self.location_id)
+        group_by = self.get_group_by(self.start_date, self.end_date)
+
+        if self.attribute_keyword:
+            keyword = "%s_%s" % (self.xform_keyword, self.attribute_keyword)
+            data = total_attribute_value_api(keyword, self.start_date, self.end_date, location, group_by_timespan=group_by['group_by'])
+        else:
+            data = total_submissions_api(self.xform_keyword, self.start_date, self.end_date, location, self.extra_filters, group_by_timespan=group_by['group_by'])
+
+        chart_data = list(data)
+        chart_data = self.reorganize_for_chart_api(group_by['group_by_name'], chart_data)
+
+        json_response_data = {'series':list(chart_data), \
+                              'timespan':group_by['group_by_name'], \
+                              'title':self.get_chart_title(), \
+                              'subtitle':self.get_chart_subtitle(), \
+                              'yaxis':self.get_y_axis(), \
+                              }
+        return json_response_data
+
+    def drill_on(self, key):
+        try:
+            self.location_id = key
+        except Location.DoesNotExist:
+            self.location_id = Location.tree.root_nodes()[0].pk
+
+
+class PercentageChartView(XFormChartView):
+    top_column = None
+    bottom_column = None
+    date_getter = XFormDateGetter()
+
+    def reorganize_for_chart_api(self, timespan, report):
+        to_ret = []
+        if len(report):
+            series = []
+            cur_loc = report[0]['location_name']
+            current_series_obj = {'name':str(cur_loc), 'data':series}
+            to_ret.append(current_series_obj)
+            for d in report:
+                if d['location_name'] != cur_loc:
+                    series = []
+                    current_series_obj = {'name':str(d['location_name']), 'data':series}
+                    to_ret.append(current_series_obj)
+                    cur_loc = d['location_name']
+                dt = d[timespan]
+                if timespan == 'month':
+                    dt = datetime.datetime(int(d['year']), int(dt), 1)
+                elif timespan == 'week':
+                    dt = datetime.datetime(int(d['year']), 1, 1) + datetime.timedelta(days=(int(dt) * 7))
+                elif timespan == 'quarter':
+                    dt = datetime.datetime(int(d['year']), int(dt) * 3, 1)
+                ts = int(time.mktime(dt.timetuple()))
+                series.append([ts, d['value']])
+
+        return to_ret
+
+
+    def get_data(self):
+        top_chart = self.top_column.get_chart().get_data()
+        bottom_chart = self.bottom_column.get_chart().get_data()
+        chart_data = []
+
+        # Iterate over the bottom data dictionary, it's our divisor
+        for bottom_data_series in bottom_chart['series']:
+            # we'll append a new series of quotients to chart_data
+            # at the end
+            series_to_add = {}
+
+            # current series we're dividing out
+            series_name = bottom_data_series['name']
+            series_to_add['name'] = series_name
+
+            # start with an empty data set
+            data_to_add = []
+            series_to_add['data'] = data_to_add
+
+            # find the corresponding series in the top data dictionary,
+            # if it exists, otherwise we end up with an empty list
+            top_data_series = {'data':[]}
+            for t in top_chart['series']:
+                if t['name'] == series_name:
+                    top_data_series = t
+                    break
+
+            bottom_counter = 0
+            top_counter = 0
+            bottom_data = bottom_data_series['data']
+            top_data = top_data_series['data']
+            # pair-wise iteration over top and bottom data, looking for
+            # matching timestamps so we can divide the values out
+            while top_counter < len(top_data) and bottom_counter < len(bottom_data):
+                top_ts = top_data[top_counter][0]
+                bottom_ts = bottom_data[bottom_counter][0]
+                if top_ts == bottom_ts:
+                    # perfect, timestamps match so we can divide this one
+                    # out and add it to data_to_add
+                    # first add the timestamp
+                    datum = [top_data[top_counter][0]]
+                    # calculate the quotient
+                    quotient = round((float(top_data[top_counter][1]) / bottom_data[bottom_counter][1]) * 100, 1)
+                    datum.append(quotient)
+                    data_to_add.append(datum)
+                    bottom_counter += 1
+                    top_counter += 1
+                elif top_ts < bottom_ts:
+                    # we're missing a bottom value for a corresponding top
+                    # that'd be a divide by zero, so we just skip it
+                    top_counter += 1
+                elif bottom_ts < top_ts:
+                    # we're missing a top value for a corresponding bottom
+                    # this is equivalent to a 0 value for the top,
+                    # so let's append that
+                    data_to_add.append([bottom_ts, 0])
+                    bottom_counter += 1
+
+            # append any leftover bottom values as zeroes, we've run out
+            # of top values
+            while bottom_counter < len(bottom_data):
+                bottom_ts = bottom_data[bottom_counter][0]
+                data_to_add.append([bottom_ts, 0])
+                bottom_counter += 1
+
+            chart_data.append(series_to_add)
+
+        group_by = self.get_group_by(self.start_date, self.end_date)
+
+        json_response_data = {'series':list(chart_data), \
+                              'timespan':group_by['group_by_name'], \
+                              'title':self.get_chart_title(), \
+                              'subtitle':self.get_chart_subtitle(), \
+                              'yaxis':self.get_y_axis(), \
+                              }
+        return json_response_data
