c1b298f8c799207a53b2106569b70c9189cb7afb
David McCann <david.a.mccann@gmail.com>
2011-11-14 08:17:39 +0300

    better signal handling

--- unregister/models.py
+++ unregister/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from rapidsms.models import Connection
+from rapidsms_httprouter.models import mass_text_sent
 
 class Blacklist(models.Model):
     connection = models.ForeignKey(Connection)
+
+def bulk_process(sender, **kwargs):
+    messages = kwargs['messages']
+    bad_conns = Blacklist.objects.values_list('connection__pk', flat=True).distinct()
+    bad_conns = Connection.objects.filter(pk__in=bad_conns)
+    messages.filter(status='P').exclude(connection__in=bad_conns).update(status='Q')
+
+mass_text_sent.connect(bulk_process, weak=False)
