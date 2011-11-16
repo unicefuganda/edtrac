36b9f7f677b88cdff04aaa92c6ac5f5a6bbec543
miclovich <vicmiclovich@gmail.com>
2011-11-16 13:35:37 +0300

    anonymous message utility for identifying messages by backend types i.e. yo8002

--- uganda_common/utils.py
+++ uganda_common/utils.py
@@ -1,13 +1,8 @@
-from .forms import DateRangeForm
-from django.conf import settings, settings
-from django.contrib.auth.models import User, Group
-from django.contrib.sites.models import Site
+from django.conf import settings
+from django.contrib.auth.models import Group
 from django.core.exceptions import ValidationError
-from django.db.models import Count, Sum, Max, Min
-from django.db.models.base import ModelBase
-from django.db.models.query import QuerySet, ValuesQuerySet
+from django.db.models import Count, Sum
 from django.http import HttpResponse
-from django.utils.text import capfirst
 from eav.models import Attribute
 from generic.utils import get_dates as get_dates_from_post
 from poll.models import Poll, LocationResponseForm, STARTSWITH_PATTERN_TEMPLATE
@@ -18,10 +13,7 @@ from rapidsms_xforms.models import XForm, XFormField, XFormFieldConstraint, \
 from script.models import Script, ScriptStep, ScriptResponse
 from script.utils.handling import find_closest_match
 import datetime
-import difflib
 import re
-import traceback
-from .forms import DateRangeForm
 from django.db.models import Max, Min
 from xlrd import open_workbook
 from rapidsms.models import Connection
@@ -424,6 +416,15 @@ def get_messages(request):
     return messages
 
 
+## TODO need a function that will get messages by connection -> for hotline, messages coming in from a particular backend
+def get_anonymous_messages(request, backend):
+    """
+    get messages coming in from polls, next is to filter by "backend" types
+    """
+    messages = Message.objects.filter(direction='I').filter(Q(application__in=["poll"])).\
+            exclude(pk__in=Response.objects.\
+            exclude(message=None).filter(has_errors=False).values_list('message_pk',flat=True))
+    return messages
 
 def parse_header_row(worksheet, fields):
 #    fields=['telephone number','name', 'district', 'county', 'village', 'age', 'gender']
