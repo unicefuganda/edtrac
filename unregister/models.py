2b2d5c5ed7ef9ae15782391a7e76015b89bec5b7
David McCann <david.a.mccann@gmail.com>
2011-12-08 10:17:32 +0300

    bulk cancel blacklisted messages

--- unregister/models.py
+++ unregister/models.py
@@ -3,3 +3,13 @@ from rapidsms.models import Connection
 
 class Blacklist(models.Model):
     connection = models.ForeignKey(Connection)
+
+def bulk_process(sender, **kwargs):
+    messages = kwargs['messages']
+    bad_conns = Blacklist.objects.values_list('connection__pk', flat=True).distinct()
+    bad_conns = Connection.objects.filter(pk__in=bad_conns)
+    messages.filter(status='P').exclude(connection__in=bad_conns).update(status='Q')
+    messages.filter(status='P').filter(connection__in=bad_conns).update(status='C')
+
+mass_text_sent.connect(bulk_process, weak=False)
+
