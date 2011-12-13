c1b733790ef026c13bd2cc1f7abde3e4754afc3f
Samuel Sekiwere <sekiskylink@gmail.com>
2011-12-13 14:15:44 +0300

    Fixed silly bug in send_qos_messages

--- rapidsms_httprouter/management/commands/send_qos_messages.py
+++ rapidsms_httprouter/management/commands/send_qos_messages.py
@@ -10,14 +10,13 @@ class Command(BaseCommand, LoggerMixin):
     help = """Sends quality of Service messages
     """     
     def send_qos_messages(self):
-        def send_qos_messages(self):
-            shortcode_backends = get_backends_by_type(btype="shortcode")
-            for si in shortcode_backends:
-                for mi in settings.ALLOWED_MODEMS[si.name]:
-                    Message.objects.create(text=gen_qos_msg(), direction='O',
-                            connection = Connection.objects.get(identity=settings.MODEM_BACKENDS[mi.name], backend=si))
-                    Message.objects.create(text=gen_qos_msg(), direction='I',
-                            connection = Connection.objects.get(identity=settings.SHORTCODE_BACKENDS[si.name], backend=mi))
+        shortcode_backends = get_backends_by_type(btype="shortcode")
+        for si in shortcode_backends:
+            for mi in settings.ALLOWED_MODEMS[si.name]:
+                Message.objects.create(text=gen_qos_msg(), direction='O',
+                        connection = Connection.objects.get(identity=settings.MODEM_BACKENDS[mi.name], backend=si))
+                Message.objects.create(text=gen_qos_msg(), direction='O',
+                        connection = Connection.objects.get(identity=settings.SHORTCODE_BACKENDS[si.name], backend=mi))
     
     def handler(self, *args, **options):
         self.send_qos_messages()
