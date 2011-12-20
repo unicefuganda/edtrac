7f38ad88371c4d48a1ef6566d4b94a476d0e6b2d
miclovich <vicmiclovich@gmail.com>
2011-12-20 10:06:34 +0300

    fix arguments

--- education/views.py
+++ education/views.py
@@ -37,7 +37,8 @@ def index(request, **kwargs):
     kwargs is where we list the variables that can be passed as our context
     use as follows: index(request, context_vars={'some_model_queryset'=Model.objects.all})
     """
-    if not kwargs['context_vars']:
+    import pdb; pdb.set_trace()
+    if not kwargs:
         return render_to_response("education/index.html", {}, RequestContext(request))
     else:
         context_vars = kwargs['context_vars']
