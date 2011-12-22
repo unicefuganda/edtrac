a42fb54ac3b33d86388df198997fcfd285a9b5b3
miclovich <vicmiclovich@gmail.com>
2011-12-22 09:26:17 +0300

    fixed message

--- education/utils.py
+++ education/utils.py
@@ -223,6 +223,6 @@ def list_poll_responses(poll):
     """
     DISTRICT = ['Kaabong', 'Kabarole', 'Kyegegwa', 'Kotido']
     for location in Location.objects.filter(name__in=DISTRICT):
-        to_ret[location.__unicode__()] = compute_average_percentage([msg.text for msg in poll.responses.filter(contact__in=Contact.objects.filter(reporting_location=location))])
+        to_ret[location.__unicode__()] = compute_average_percentage([msg.message.text for msg in poll.responses.filter(contact__in=Contact.objects.filter(reporting_location=location))])
     return to_ret
 
