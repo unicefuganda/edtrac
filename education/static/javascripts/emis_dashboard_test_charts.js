2b4c7c878ad9bc3a1874ce981e7ccaa3c3f7eb9d
miclovich <vicmiclovich@gmail.com>
2011-12-27 09:50:35 +0300

    progress bar in progress

--- education/static/javascripts/emis_dashboard_test_charts.js
+++ education/static/javascripts/emis_dashboard_test_charts.js
@@ -1,4 +1,6 @@
-function smc_meetings() {
+function smc_meetings(schools, meetings) {
+    var schools;
+    var meetings;
     meeting_chart = new Highcharts.Chart({
         chart: {
             renderTo: 'smc_meetings',
@@ -145,84 +147,3 @@ function lunch(data) {
         }]
     });
 }
-
-function progress(){
-    $.fn.animateProgress = function(progress, callback){
-        return this.each(function(){
-            $(this).animate(
-                {
-                    width:progress + '%'
-                },
-                {
-                    duration:2000,
-                    easing:'swing',
-                    step: function(progress){
-                        var labelE1 = $('.ui_label', this), valueE1 = $('.value', labelE1);
-                        if (Math.ceil(progress) < 20 && $('.ui_label',this).is(":visible")){
-                            labelE1.hide();
-                        } else {
-                            if (labelE1.is(":hidden")){
-                                labelE1.fadeIn();
-                            };
-                        }
-
-                        if (Math.ceil(progress) == 100){
-                            labelE1.text('Finished');
-                            // timeout might not be necessary
-                            setTimeout(function(){
-                                labelE1.fadeOut();
-                            }, 1000);
-                        } else {
-                            valueE1.text(Math.ceil(progress) + "%");
-                        }
-                    },
-                    complete: function(scope, i, elem){
-                        if (callback){
-                            callback.call(this, i, elem);
-                        };
-                    }
-                }); // end animate function
-        });} // end animate progress loop
-}
-
-
-
-
-(function( $ ){
-    $.fn.animateProgress = function(progress, callback){
-	return this.each(function(){
-		$(this).animate(
-			{
-			width:progress + '%'
-			}, 
-			{
-			duration:2000,
-			easing:'swing',
-			step: function(progress){
-				var labelE1 = $('.ui_label', this), valueE1 = $('.value', labelE1);
-				if (Math.ceil(progress) < 20 && $('.ui_label',this).is(":visible")){
-					labelE1.hide();
-				} else {
-					if (labelE1.is(":hidden")){
-						labelE1.fadeIn();
-					};
-				}
-				
-				if (Math.ceil(progress) == 100){
-					labelE1.text('Finished');
-					// timeout might not be necessary
-					setTimeout(function(){
-						labelE1.fadeOut();						
-					}, 1000);
-				} else {
-					valueE1.text(Math.ceil(progress) + "%");
-				}								
-			},
-			complete: function(scope, i, elem){
-				if (callback){
-					callback.call(this, i, elem);
-				};
-			}
-		}); // end animate function
-	});} // end animate progress loop
-});
