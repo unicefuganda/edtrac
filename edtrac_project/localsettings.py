b8a08b0e21d3b415fdbe70a0c0322cad3a246811
miclovich <vicmiclovich@gmail.com>
2012-01-04 15:15:17 +0300

    fix override

--- edtrac_project/localsettings.py
+++
@@ -1,8 +0,0 @@
-DATABASES = {
-    'default': {
-        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'emis',
-        'USER': 'postgres',
-        'HOST': 'localhost',
-        }
-}
