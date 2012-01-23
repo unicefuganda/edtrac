99d67c49c1a7012a24154665a57c896b01830ef4
miclovich <vicmiclovich@gmail.com>
2012-01-23 14:22:28 +0300

    created management command use as follows ./manage.py prod_fake_responses 'poll_name' 'group_name'

--- education/management/commands/prod_fake_responses.py
+++ education/management/commands/prod_fake_responses.py
@@ -1,25 +1,25 @@
 from django.core.management.base import BaseCommand
+from poll.models import Poll
+from rapidsms.messages.incoming import IncomingMessage
+from education.models import EmisReporter
+from rapidsms_httprouter.models import Message
+import random
+
 
 def fake_incoming_message(message, connection):
-    from rapidsms.messages.incoming import IncomingMessage
     incomingmessage = IncomingMessage(connection, message)
     incomingmessage.db_message = Message.objects.create(direction='I', connection=connection, text=message)
     return incomingmessage
 
 def fake_poll_responses(poll_tuple, grp):
-    from education.models import EmisReporter
-    import random
     yesno_resp = ['yes', 'no']
-
     text_resp = ['0%', '25%', '50%', '75%', '100%']
-
     poll = Poll.objects.get(name=poll_tuple[1])
     rep_count = EmisReporter.objects.filter(groups__name=grp).count()
 
     for rep in EmisReporter.objects.filter(groups__name=grp):
         if rep.default_connection:
             if poll_tuple[0] == Poll.TYPE_NUMERIC:
-                import pdb; pdb.set_trace()
                 poll.process_response(fake_incoming_message('%s' % random.randint(0,90), rep.default_connection))
             elif poll_tuple[0] == Poll.TYPE_TEXT:
             #            if poll.categories.values_list('name', flat=True)[0] in ['yes', 'no', 'unknown']:
@@ -34,13 +34,11 @@ def fake_poll_responses(poll_tuple, grp):
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        poll_name = options.get('poll_name')
-        grp = options.get('group')
+        poll_name = args[0]
+        grp = args[1]
         poll = Poll.objects.get(name=poll_name)
-        from poll.models import Poll
-        self.stdout('about to start creating fake responses')
         fake_poll_responses(
             (poll.type, poll.name),
             grp
         )
-        self.stdout('finished creating fake responses...!')
+        print "finished creating responses"
--- education/utils.py
+++ education/utils.py
@@ -321,34 +321,3 @@ themes = {
     12.2: 'Ways of saving energy',
     12.3: 'Dangers of energy and ways of avoiding them'
 }
-
-def fake_incoming_message(message, connection):
-    from rapidsms.messages.incoming import IncomingMessage
-    incomingmessage = IncomingMessage(connection, message)
-    incomingmessage.db_message = Message.objects.create(direction='I', connection=connection, text=message)
-    return incomingmessage
-    
-def fake_poll_responses(poll_tuple, grp):
-    from education.models import EmisReporter
-    import random
-    yesno_resp = ['yes', 'no']
-
-    text_resp = ['0%', '25%', '50%', '75%', '100%']
-
-    poll = Poll.objects.get(name=poll_tuple[1])
-    rep_count = EmisReporter.objects.filter(groups__name=grp).count()
-
-    for rep in EmisReporter.objects.filter(groups__name=grp):
-        if rep.default_connection:
-            if poll_tuple[0] == Poll.TYPE_NUMERIC:
-                poll.process_response(fake_incoming_message('%s' % random.randint(0,90), rep.default_connection))
-            elif poll_tuple[0] == Poll.TYPE_TEXT:
-    #            if poll.categories.values_list('name', flat=True)[0] in ['yes', 'no', 'unknown']:
-    #                resp = random.choice(yesno_resp)
-    #            else:
-                resp = random.choice(text_resp)
-                poll.process_response(fake_incoming_message(resp, rep.default_connection))
-            elif poll_tuple[0] == Poll.TYPE_CHOICES:
-                pass
-        else:
-            pass
