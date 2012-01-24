905aaa823695b3508485316ce0883a7150cca2f9
miclovich <vicmiclovich@gmail.com>
2012-01-24 16:09:12 +0300

    corrected management utility function

--- education/management/commands/prod_fake_responses.py
+++ education/management/commands/prod_fake_responses.py
@@ -21,17 +21,21 @@ def fake_poll_responses(poll_tuple, grp):
     text_resp = ['0%', '25%', '50%', '75%', '100%']
     poll = Poll.objects.get(name=poll_tuple[1])
     rep_count = EmisReporter.objects.filter(groups__name=grp).count()
+
     for rep in EmisReporter.objects.filter(groups__name=grp):
-        if poll_tuple[0] == Poll.TYPE_NUMERIC:
-            poll.process_response(fake_incoming_message('%s' % random.randint(0,90), rep.default_connection))
-        elif poll_tuple[0] == Poll.TYPE_TEXT:
+        if not rep.default_connection == None:
+            if poll_tuple[0] == Poll.TYPE_NUMERIC:
+                poll.process_response(fake_incoming_message('%s' % random.randint(0,90), rep.default_connection))
+            elif poll_tuple[0] == Poll.TYPE_TEXT:
             #            if poll.categories.values_list('name', flat=True)[0] in ['yes', 'no', 'unknown']:
             #                resp = random.choice(yesno_resp)
             #            else:
-            resp = random.choice(text_resp)
-            poll.process_response(fake_incoming_message(resp, rep.default_connection))
-        elif poll_tuple[0] == Poll.TYPE_CHOICES:
-            pass
+                resp = random.choice(text_resp)
+                poll.process_response(fake_incoming_message(resp, rep.default_connection))
+            elif poll_tuple[0] == Poll.TYPE_CHOICES:
+                pass
+        #just ignore folks with no default connection
+        pass
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
