4a7123018baa5f05182641790d8a952eded1efa4
Samuel Sekiwere <sekiskylink@gmail.com>
2012-02-03 12:48:30 +0300

    Made support of seperate DB monitor

--- rapidsms_httprouter/management/commands/send_qos_messages.py
+++ rapidsms_httprouter/management/commands/send_qos_messages.py
@@ -1,23 +1,23 @@
 from django.conf import settings
 from django.core.management import BaseCommand
 from django.core.mail import send_mail
-from rapidsms.models import Connection,Backend
+from rapidsms.models import Connection, Backend
 from rapidsms_httprouter.models import Message
 from rapidsms.log.mixin import LoggerMixin
-from rapidsms_httprouter.qos_messages import get_backends_by_type,gen_qos_msg
+from rapidsms_httprouter.qos_messages import get_backends_by_type, gen_qos_msg
 
 class Command(BaseCommand, LoggerMixin):
     help = """Sends quality of Service messages
-    """     
+    """
     def send_qos_messages(self):
-        shortcode_backends = get_backends_by_type(btype="test")
+        shortcode_backends = get_backends_by_type(btype=getattr(settings, 'QOS_BACKEND_TYPE', 'shortcode'))
         for si in shortcode_backends:
             for mi in settings.ALLOWED_MODEMS[si.name]:
-                (mb,t) = Backend.objects.get_or_create(name=mi)
+                (mb, t) = Backend.objects.get_or_create(name=mi)
+                Message.objects.using('monitor').create(text=gen_qos_msg(), direction='O',
+                        connection=Connection.objects.using('monitor').get_or_create(identity=settings.MODEM_BACKENDS[mb.name], backend=si)[0])
                 Message.objects.create(text=gen_qos_msg(), direction='O',
-                        connection = Connection.objects.get_or_create(identity=settings.MODEM_BACKENDS[mb.name], backend=si)[0])
-                Message.objects.create(text=gen_qos_msg(), direction='O',
-                        connection = Connection.objects.get_or_create(identity=settings.SHORTCODE_BACKENDS[si.name], backend=mb)[0])
-    
+                        connection=Connection.objects.using('monitor').get_or_create(identity=settings.SHORTCODE_BACKENDS[si.name], backend=mb)[0])
+
     def handle(self, *args, **options):
         self.send_qos_messages()
--- rapidsms_httprouter/qos_messages.py
+++ rapidsms_httprouter/qos_messages.py
@@ -7,25 +7,14 @@ from rapidsms.log.mixin import LoggerMixin
 from datetime import datetime, timedelta
 
 def get_backends_by_type(btype='shortcode'):
-        backends = []
-        for db in settings.DATABASES.keys():
-            if db == 'default':
-                continue
-            bs = list(Backend.objects.using(db).exclude(name='console').order_by('name').values_list('name', flat=True))
-            for b in bs:
-                if b not in backends:
-                    backends.append(b)
-
         if btype == 'shortcode':
             # messenger's DB has all backends from all other deployments
-            return Backend.objects.using('default').filter(name__in=backends).order_by('name')
+            return Backend.objects.using('monitor').exclude(name__endswith='modem').order_by('name')
         elif btype == 'modem':
-            backends.append('console')
-            return Backend.objects.using('default').exclude(name__in=backends).order_by('name')
+            return Backend.objects.using('monitor').filter(name__endswith='modem').order_by('name')
         else:
             return [Backend.objects.get_or_create(name="test_backend")[0]]
 
-        
 def gen_qos_msg():
     return datetime.now().strftime('%Y-%m-%d %H')
 
@@ -40,15 +29,15 @@ def get_recipients():
         for email in mgr:
             recipients.append(email)
     return recipients
-    
+
 def get_qos_time_offset():
-    qos_interval = getattr(settings,'QOS_INTERVAL', {'hours':1,'minutes':0,'offset':5})
-    time_offset = datetime.now() - timedelta(hours=qos_interval['hours'],minutes=(qos_interval['minutes']+qos_interval['offset']))
+    qos_interval = getattr(settings, 'QOS_INTERVAL', {'hours':1, 'minutes':0, 'offset':5})
+    time_offset = datetime.now() - timedelta(hours=qos_interval['hours'], minutes=(qos_interval['minutes'] + qos_interval['offset']))
     return time_offset
 
 def get_alarms(mode="shortcode"):
     # here mode refers to test mode which returns test backends
-    if mode =="test": 
+    if mode == "test":
         btype = "test"
     else:
         btype = mode
@@ -57,18 +46,18 @@ def get_alarms(mode="shortcode"):
     time_offset = get_qos_time_offset()
     for si in shortcode_backends:
         for mi in settings.ALLOWED_MODEMS[si.name]:
-            (mb,t) = Backend.objects.get_or_create(name=mi)
-            
-            b = Message.objects.filter(date__gt=time_offset, direction='I',text=gen_qos_msg(),
-                    connection=Connection.objects.get_or_create(identity=settings.SHORTCODE_BACKENDS[si.name], backend=mb)[0])
+            (mb, t) = Backend.objects.get_or_create(name=mi)
+
+            b = Message.objects.using('monitor').filter(date__gt=time_offset, direction='I', text=gen_qos_msg(),
+                    connection=Connection.objects.using('monitor').get_or_create(identity=settings.SHORTCODE_BACKENDS[si.name], backend=mb)[0])
             if not b.count():
-                msg = "Could not get response from %s when sender is %s Backend"%(settings.SHORTCODE_BACKENDS[si.name],mb.name)
+                msg = "Could not get response from %s when sender is %s Backend" % (settings.SHORTCODE_BACKENDS[si.name], mb.name)
                 msgs.append(msg)
-                    
-            
-            b = Message.objects.filter(date__gt=time_offset, direction='I',text=gen_qos_msg(),
-                    connection=Connection.objects.get_or_create(identity=settings.MODEM_BACKENDS[mi], backend=si)[0])
+
+
+            b = Message.objects.using('monitor').filter(date__gt=time_offset, direction='I', text=gen_qos_msg(),
+                    connection=Connection.objects.using('monitor').get_or_create(identity=settings.MODEM_BACKENDS[mi], backend=si)[0])
             if not b.count():
-                msg = "Could not get response from %s when sender is %s Backend"%(settings.MODEM_BACKENDS[mi],si.name)
+                msg = "Could not get response from %s when sender is %s Backend" % (settings.MODEM_BACKENDS[mi], si.name)
                 msgs.append(msg)
     return msgs
