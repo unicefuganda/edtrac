420aeeac49967bc444da3e44bcf4b145208296be
miclovich <vicmiclovich@gmail.com>
2012-02-09 16:14:38 +0300

    quick fix

--- education/app.py
+++ education/app.py
@@ -12,62 +12,61 @@ class App (AppBase):
         #validation
         # the assumption is that messages that are sent in are not more than 1 valued responses
         # examples of such messages from the xform circa which isn't used much in EduTra: Teachers M 3 F 4
-        if len(message.text.strip().lower().strip()) > 1:
-            #TODO: handle situations where user is expected to send in a text with a response like ``I don't know``
-            message.respond("You have sent an invalid message to EduTrac (6200). Please send only one value as a response to the question.")
-            return
-        else:
-            if message.text.strip().lower() in [i.lower() for i in getattr(settings, 'OPT_OUT_WORDS', ['quit'])] or\
-                levenshtein(message.text.replace('.', '').strip().lower(), 'quit') < 3:
+#        if len(message.text.strip().lower().strip()) > 1:
+#            message.respond("You have sent an invalid message. Please send only one value")
+#            return
+#        else:
+        if message.text.strip().lower() in [i.lower() for i in getattr(settings, 'OPT_OUT_WORDS', ['quit'])] or\
+            levenshtein(message.text.replace('.', '').strip().lower(), 'quit') < 3:
 
-                if Blacklist.objects.filter(connection=message.connection).exists():
-                        message.respond('You cannot send Quit to 6200 (EduTrac) more than once.')
-                        return
+            if Blacklist.objects.filter(connection=message.connection).exists():
+                    message.respond('You cannot send Quit to 6200 (EduTrac) more than once.')
+                    return
+            else:
+                # create a Blacklist object
+                Blacklist.objects.create(connection=message.connection)
+                if (message.connection.contact):
+                    reporter = EmisReporter.objects.get(connection=message.connection)
+                    message.connection.contact.active = False
+                    message.connection.contact.save()
+                    reporter.active = False
+                    reporter.save()
+                message.respond(getattr(settings, 'OPT_OUT_CONFIRMATION', 'Thank you for your contribution to EduTrac. To rejoin the system, send join to 6200'))
+                return True
+
+        elif message.text.strip().lower() in [i.lower() for i in getattr(settings, 'OPT_IN_WORDS', ['join'])] or\
+            levenshtein(message.text.replace('.', '').strip().lower(), 'join') < 3:
+            # check if incoming connection is Blacklisted (previously quit)
+            if Blacklist.objects.filter(connection=message.connection).exists():
+                Blacklist.objects.filter(connection=message.connection).delete()
+                # create a ScriptProgress object if it does not exist
+                # this throws the user back to autoreg
+                if not ScriptProgress.objects.filter(script__slug='edtrac_autoreg', connection=message.connection).exists():
+                    ScriptProgress.objects.create(script=Script.objects.get(slug="edtrac_autoreg"),\
+                        connection=message.connection, language="en")
                 else:
-                    # create a Blacklist object
-                    Blacklist.objects.create(connection=message.connection)
-                    if (message.connection.contact):
-                        reporter = EmisReporter.objects.get(connection=message.connection)
-                        message.connection.contact.active = False
-                        message.connection.contact.save()
-                        reporter.active = False
-                        reporter.save()
-                    message.respond(getattr(settings, 'OPT_OUT_CONFIRMATION', 'Thank you for your contribution to EduTrac. To rejoin the system, send join to 6200'))
-                    return True
+                    # what if we deleted existing progresses and recreated them?
+                    ScriptProgress.objects.filter(script=Script.objects.get(slug="edtrac_autoreg"),\
+                        connection = message.connection, language="en").delete()
+                    # recreating or getting an existing in case it creeps in
+                    ScriptProgress.objects.get_or_create(script=Script.objects.get(slug="edtrac_autoreg"),\
+                        connection=message.connection, language="en")
 
-            elif message.text.strip().lower() in [i.lower() for i in getattr(settings, 'OPT_IN_WORDS', ['join'])] or\
-                levenshtein(message.text.replace('.', '').strip().lower(), 'join') < 3:
-                # check if incoming connection is Blacklisted (previously quit)
-                if Blacklist.objects.filter(connection=message.connection).exists():
-                    Blacklist.objects.filter(connection=message.connection).delete()
-                    # create a ScriptProgress object if it does not exist
-                    # this throws the user back to autoreg
-                    if not ScriptProgress.objects.filter(script__slug='edtrac_autoreg', connection=message.connection).exists():
-                        ScriptProgress.objects.create(script=Script.objects.get(slug="edtrac_autoreg"),\
-                            connection=message.connection, language="en")
-                    else:
-                        # what if we deleted existing progresses and recreated them?
-                        ScriptProgress.objects.filter(script=Script.objects.get(slug="edtrac_autoreg"),\
-                            connection = message.connection, language="en").delete()
-                        # recreating or getting an existing in case it creeps in
-                        ScriptProgress.objects.get_or_create(script=Script.objects.get(slug="edtrac_autoreg"),\
-                            connection=message.connection, language="en")
-
-                # For users joining edtrac the first time
+            # For users joining edtrac the first time
+            else:
+                # check that the user is not in the system
+                if not message.connection.contact:
+                    # dump the user into autoreg
+                    ScriptProgress.objects.get_or_create(script=Script.objects.get(slug="edtrac_autoreg"), \
+                        connection=message.connection, language="en")
                 else:
-                    # check that the user is not in the system
-                    if not message.connection.contact:
-                        # dump the user into autoreg
-                        ScriptProgress.objects.get_or_create(script=Script.objects.get(slug="edtrac_autoreg"), \
-                            connection=message.connection, language="en")
-                    else:
+
                         # otherwise, chances are this user already exists in the system
                         message.respond("You are already in the system and do not need to 'Join' again.")
-                    # quit this.
+                # quit this.
                 return True
 
             elif Blacklist.objects.filter(connection=message.connection).count():
                 return True
             # when all else fails, quit!
-            else:
-                return False
+            return False
