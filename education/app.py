16dc2b1e02445ce69ce496eb3c0dbdedcb553b9a
miclovich <vicmiclovich@gmail.com>
2012-03-02 15:00:07 +0300

    commit internal

--- education/app.py
+++ education/app.py
@@ -30,7 +30,7 @@ class App (AppBase):
                     message.connection.contact.active = False
                     message.connection.contact.save()
                 message.respond(getattr(settings, 'OPT_OUT_CONFIRMATION', 'Thank you for your contribution to EduTrac. To rejoin the system, send join to 6200'))
-                return
+                return True
             #                return True
 
         elif message.text.strip().lower() in [i.lower() for i in getattr(settings, 'OPT_IN_WORDS', ['join'])] or\
@@ -41,30 +41,16 @@ class App (AppBase):
                 Blacklist.objects.filter(connection=message.connection).delete()
                 # create a ScriptProgress object if it does not exist
                 # this throws the user back to autoreg
-                if not ScriptProgress.objects.filter(script__slug='edtrac_autoreg', connection=message.connection).exists():
-                    ScriptProgress.objects.create(script=Script.objects.get(slug="edtrac_autoreg"),\
-                        connection=message.connection, language="en")
-                else:
-                    # what if we deleted existing progresses and recreated them?
-                    ScriptProgress.objects.filter(script=Script.objects.get(slug="edtrac_autoreg"),\
-                        connection = message.connection, language="en").delete()
-                    # recreating or getting an existing in case it creeps in
-                    ScriptProgress.objects.get_or_create(script=Script.objects.get(slug="edtrac_autoreg"),\
-                        connection=message.connection, language="en")
-            # For users joining edtrac the first time
+            if not ScriptProgress.objects.filter(script__slug='edtrac_autoreg', connection=message.connection).exists():
+                ScriptProgress.objects.create(script=Script.objects.get(slug="edtrac_autoreg"),\
+                    connection=message.connection, language="en")
             else:
-                # check that the user is not in the system
-                if not message.connection.contact:
-                    # dump the user into autoreg
-                    ScriptProgress.objects.get_or_create(script=Script.objects.get(slug="edtrac_autoreg"),\
-                        connection=message.connection, language="en")
-                else:
-                    # otherwise, chances are this user already exists in the system
-                    message.respond("You are already in the system and do not need to 'Join' again.")
-                    # quit this.
+                # otherwise, chances are this user already exists in the system
+                message.respond("You are already in the system and do not need to 'Join' again.")
+                # quit this.
             return True
 
         elif Blacklist.objects.filter(connection=message.connection).count():
             return True
             # when all else fails, quit!
-        return
+        return False
