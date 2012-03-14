91fda7e0917559ab7eb70b2a7665658a03b6b496
Alfred Assey M <asseym@gmail.com>
2012-03-14 15:57:26 +0300

    added management command for reschedulling weekly polls

---
+++ education/management/commands/reschedule_weekly_polls.py
@@ -0,0 +1,23 @@
+'''
+Created on Nov 1, 2011
+
+@author: asseym
+'''
+
+from django.core.management.base import BaseCommand
+from education.models import reschedule_weekly_polls
+from optparse import OptionParser, make_option
+
+class Command(BaseCommand):
+    
+    option_list = BaseCommand.option_list + (
+        make_option("-g", "--group", dest="group"),
+    )
+    def handle(self, **options):
+        if not options['group']:
+            group = raw_input('For which group? -- Teahers, SMC, Head Teachers:')
+        else:
+            group = options['group']
+        reschedule_weekly_polls(group)
+        self.stdout.write('')
+        self.stdout.write('Done')
