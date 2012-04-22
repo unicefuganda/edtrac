fdde3e29e7514bfa0777ee80921ba0f7f17c3a63
Alfred Assey M <asseym@gmail.com>
2012-04-22 17:18:14 +0300

    clean up

--- rapidsms_httprouter/management/commands/send_messages.py
+++ rapidsms_httprouter/management/commands/send_messages.py
@@ -122,15 +122,12 @@ class Command(BaseCommand, LoggerMixin):
             for db in DBS:
                 try:
                     self.debug("servicing db '%s'" % db)
-                    self.info("servicing db '%s'" % db)
                     router_url = settings.DATABASES[db]['ROUTER_URL']
                     transaction.enter_transaction_management(using=db)
                     self.db = db
                     to_process = MessageBatch.objects.using(db).filter(status='Q')
                     self.debug("looking for batch messages to process")
-                    self.info("looking for batch messages to process")
                     if to_process.count():
-                        self.info("%s batches found in %s" (to_process.count(), db))
                         batch = to_process[0]
                         to_process = batch.messages.using(db).filter(direction='O',
                                       status__in=['Q']).order_by('priority', 'status', 'connection__backend__name')[:CHUNK_SIZE]
