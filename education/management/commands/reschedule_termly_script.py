b7582273073c1519ef8eb4af0e9816d349277533
Ray Besiga <raybesiga@gmail.com>
2013-02-19 16:26:10 +0300

    Edit reschedule termly script

--- education/management/commands/reschedule_termly_script.py
+++ education/management/commands/reschedule_termly_script.py
@@ -7,6 +7,10 @@ Created on Feb 8, 2013
 from django.core.management.base import BaseCommand
 from education.models import reschedule_termly_script
 from optparse import OptionParser, make_option
+from education.utils import _next_term_question_date
+from rapidsms.models import Connection
+from unregister.models import Blacklist
+from script.models import Script, ScriptProgress
 
 class Command(BaseCommand):
 
@@ -30,6 +34,38 @@ class Command(BaseCommand):
         else:
             slug = 'edtrac_p3_enrollment_headteacher_termly'
 
-        reschedule_termly_script(grp=group, date=date, slug=slug)
-        self.stdout.write('')
-        self.stdout.write('done!')
+#        reschedule_termly_script(grp=group, date=date, slug=slug)
+#        self.stdout.write('')
+#        self.stdout.write('done!')
+        
+        connections = Connection.objects.filter(contact__emisreporter__groups__name__in=[group])
+        script = Script.objects.get(slug=slug)
+        for connection in connections:
+            try:
+                Blacklist.objects.get(connection=connection)
+                print '%s blacklisted! ' % connection.identity
+            except Blacklist.DoesNotExist:
+                try:
+                    ScriptProgress.objects.get(connection=connection, script=script)
+                except ScriptProgress.DoesNotExist:
+                    if date:
+                        d = _next_term_question_date(date=date)
+                    else:
+                        if group == 'SMC':
+                            d = _next_term_question_date(True)
+                        else:
+                            d = _next_term_question_date(False)
+                            
+                    sp = ScriptProgress.objects.create(script=script, connection=connection)
+                    sp.set_time(d)
+                    print 'ScriptProgress for %s created!!' % connection
+                except ScriptProgress.MultipleObjectsReturned:
+                    sps = ScriptProgress.objects.filter(connection=connection, script=script)
+                    i = 0
+                    for i in range(1, len(sps)):
+                        sps[i].delete()
+                        print 'Duplicate progress for %s deleted!!' % connection
+        
+        self.stdout.write('\n')
+        self.stdout.write('Done')
+        
