47a3a8ce299dc8ae76c20ccdfcd21228a5035058
Akshay Naval <akshay@akshaynaval.(none)>
2013-04-15 17:08:06 +0530

    Revert "Change reschedule_monthly_poll logic"
    This reverts commit 61a9675fcc3c4eb922f48fefe6a2d198eda2134b.

--- education/management/commands/reschedule_monthly_polls.py
+++ education/management/commands/reschedule_monthly_polls.py
@@ -6,17 +6,8 @@ Created on Nov 1, 2011
 
 from django.core.management.base import BaseCommand
 from education.models import reschedule_monthly_polls
-from optparse import OptionParser, make_option
-
 
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
-        make_option("-g", "--group", dest="group"),
-    )
     def handle(self, **options):
-        if not options['group']:
-            group = raw_input('Group -- GEM, SMC or Head Teachers:')
-        else:
-            group = 'all'
-        reschedule_monthly_polls(grp=group)
+        reschedule_monthly_polls()
         self.stdout.write('done!')
