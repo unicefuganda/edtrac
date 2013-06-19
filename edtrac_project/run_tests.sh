b0f443881be88d216cf804089bebba8f8083af7a
Kisitu Augustine <austiine04@gmail.com>
2013-06-19 18:44:04 +0300

    changed database name and specified which db to run migrations on

--- edtrac_project/run_tests.sh
+++ edtrac_project/run_tests.sh
@@ -4,7 +4,7 @@ echo "Synchronizing database ............... "
 python manage.py syncdb --noinput
 
 echo "Running migrations ................... "
-python manage.py migrate
+python manage.py migrate --database=test_edtrac
 
 echo "Running tests ........................ "
 python manage.py test education.test
