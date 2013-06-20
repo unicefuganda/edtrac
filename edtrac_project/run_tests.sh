fd04ad7b8e3ecc5a54930cfffaf6b5e1b20da9e4
Kisitu Augustine <austiine04@gmail.com>
2013-06-20 12:27:27 +0300

    added travis settings

--- edtrac_project/run_tests.sh
+++ edtrac_project/run_tests.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
-echo "Synchronizing database ............... "
-#python manage.py syncdb --noinput --database=test
 
-echo "Running migrations ................... "
-#python manage.py migrate --database=test
+
+psql -c 'create database test_edtrac;' -U postgres
+
+echo "Setting up test database ................... "
+python manage.py syncdb --noinput --database=test
+python manage.py migrate --database=test
 
 echo "Running tests ........................ "
-python manage.py test education.test
+python manage.py test education.test REUSE_DB=1
