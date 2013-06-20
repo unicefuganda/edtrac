3176e2d87fc17e1b766c668c94895908e9920770
Kisitu Augustine <austiine04@gmail.com>
2013-06-20 11:10:26 +0300

    changed script to only run tests

--- edtrac_project/run_tests.sh
+++ edtrac_project/run_tests.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 echo "Synchronizing database ............... "
-python manage.py syncdb --noinput
+#python manage.py syncdb --noinput --database=test
 
 echo "Running migrations ................... "
-python manage.py migrate --database=test_edtrac
+#python manage.py migrate --database=test
 
 echo "Running tests ........................ "
 python manage.py test education.test
