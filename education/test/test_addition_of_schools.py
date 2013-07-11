4978ac5aa466afc7a6f832a6ed0cd1a7bfec27c2
Kisitu Augustine <austiine04@gmail.com>
2013-07-11 10:55:23 +0300

    removed duplicated code from add_school tests

--- education/test/test_addition_of_schools.py
+++ education/test/test_addition_of_schools.py
@@ -34,6 +34,11 @@ class TestAdditionOfSchools(TestCase):
         self.kampala_school = create_school("St. Joseph's", self.kampala_district)
         self.admin_user = create_user_with_group("John", admin_group, self.uganda)
 
+    def __http_post(self, data={}):
+        client = Client()
+        client.login(username='John', password='password')
+        client.post('/edtrac/add_schools/', data)
+
     def test_that_one_school_has_been_created_in_setup(self):
         self.assertEqual(1, School.objects.all().count())
 
@@ -41,33 +46,25 @@ class TestAdditionOfSchools(TestCase):
         self.assertTrue("St. Joseph's" in School.objects.all().values_list('name', flat=True))
 
     def test_adding_one_school_should_increase_numbers_of_schools_by_one(self):
-        client = Client()
-        client.login(username='John', password='password')
-        client.post('/edtrac/add_schools/', {'name': 'School2', 'location': self.kampala_district.id})
+        school = {'name':"school2", 'location' : self.kampala_district.id}
+        self.__http_post(school)
         self.assertEqual(2, School.objects.all().count())
         self.assertTrue("School1", School.objects.all().values_list('name', flat=True))
 
     def test_adding_two_schools_should_increase_number_of_schools_by_two(self):
-        client = Client()
-        client.login(username='John', password='password')
         schools = {'name': ['School2', 'school3'], 'location': [self.kampala_district.id, self.kampala_district.id]}
-        client.post('/edtrac/add_schools/', schools)
+        self.__http_post(schools)
         self.assertEquals(3, School.objects.all().count())
         self.assertTrue("school3" in School.objects.all().values_list('name', flat=True))
         self.assertTrue("School2" in School.objects.all().values_list('name', flat=True))
 
     def test_adding_no_school_should_not_increase_number_of_schools(self):
-        client = Client()
-        client.login(username='John', password='password')
-        schools = {}
-        client.post('/edtrac/add_schools/', schools)
+        self.__http_post()
         self.assertEquals(1, School.objects.all().count())
 
     def test_add_school_without_location_shouldnot_increase_number_of_schools(self):
-        client = Client()
-        client.login(username='John', password='password')
         schools = {'name':"school2"}
-        client.post('/edtrac/add_schools/', schools)
+        self.__http_post(schools)
         self.assertEquals(1, School.objects.all().count())
         self.assertFalse("school2" in School.objects.all())
 
