0e9c5347227c32ab03c7f80e6c65174585f65faa
Martin Atukunda <matlads@gmail.com>
2013-09-27 10:26:07 +0300

    Show the location for Schools in the admin interface

--- education/admin.py
+++ education/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from .models import Role, UserProfile, School, EmisReporter
 
+
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ['name', 'location']
+
+
 admin.site.register(Role)
-admin.site.register(School)
+admin.site.register(School, SchoolAdmin)
 admin.site.register(UserProfile)
 admin.site.register(EmisReporter)
