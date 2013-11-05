e415fb8f3ca971f785dc7807086ad53b6db2c0fb
Chris Ford <christophertford@gmail.com>
2013-11-05 16:25:59 +0300

    Test that invalid date ranges are detected.

--- education/test/test_absenteeism_form.py
+++ education/test/test_absenteeism_form.py
@@ -12,6 +12,10 @@ class TestAbsenteeismForm(TestCase):
         absenteeism_form = AbsenteeismForm(data={'to_date':'12/12/2012', 'from_date':'12/14/2012'})
         self.assertFalse(absenteeism_form.is_valid())
 
+    def test_should_invalidate_if_from_date_is_greater_than_to_date(self):
+        absenteeism_form = AbsenteeismForm(data={'from_date':'12/12/2012', 'to_date':'12/14/2012'})
+        self.assertTrue(absenteeism_form.is_valid())
+
     def test_should_get_cleaned_data_after_validation(self):
         absenteeism_form = AbsenteeismForm(data={'to_date':'12/21/2012', 'from_date':'12/14/2012', 'indicator':'all'})
         self.assertTrue(absenteeism_form.is_valid())
