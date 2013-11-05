53f689152c4d5b814ac29f3cabca034a177afc45
Chris Ford <christophertford@gmail.com>
2013-11-05 16:08:20 +0300

    Fixed typo in variable name.

--- education/test/test_absenteeism_form.py
+++ education/test/test_absenteeism_form.py
@@ -5,16 +5,16 @@ from datetime import datetime
 
 class TestAbsenteeismForm(TestCase):
     def test_should_invalidate_empty_form(self):
-        absenteeism_from = AbsenteeismForm(data={})
-        self.assertFalse(absenteeism_from.is_valid())
+        absenteeism_form = AbsenteeismForm(data={})
+        self.assertFalse(absenteeism_form.is_valid())
 
     def test_should_validate_if_to_date_is_greater_than_from_date(self):
-        absenteeism_from = AbsenteeismForm(data={'to_date':'12/12/2013', 'from_date':'12/14/2013'})
-        self.assertFalse(absenteeism_from.is_valid())
+        absenteeism_form = AbsenteeismForm(data={'to_date':'12/12/2013', 'from_date':'12/14/2013'})
+        self.assertFalse(absenteeism_form.is_valid())
 
     def test_should_get_cleaned_data_after_validation(self):
-        absenteeism_from = AbsenteeismForm(data={'to_date':'12/21/2013', 'from_date':'12/14/2013', 'indicator':'all'})
-        self.assertTrue(absenteeism_from.is_valid())
-        self.assertEqual(datetime(2013,12,21), absenteeism_from.cleaned_data['to_date'])
-        self.assertEqual(datetime(2013,12,14), absenteeism_from.cleaned_data['from_date'])
-        self.assertEqual('all', absenteeism_from.cleaned_data['indicator'])
+        absenteeism_form = AbsenteeismForm(data={'to_date':'12/21/2013', 'from_date':'12/14/2013', 'indicator':'all'})
+        self.assertTrue(absenteeism_form.is_valid())
+        self.assertEqual(datetime(2013,12,21), absenteeism_form.cleaned_data['to_date'])
+        self.assertEqual(datetime(2013,12,14), absenteeism_form.cleaned_data['from_date'])
+        self.assertEqual('all', absenteeism_form.cleaned_data['indicator'])
