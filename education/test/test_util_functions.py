18deb3785571a4020f1f12f64161fdc77bf4975d
Neil Coutinho <ncoutin@thoughtworks.com>
2013-11-13 16:39:15 +0300

    Updating the test util functions to utilise a hard coded date rather tha today's date in creating the datetime object used in the tests

--- education/test/test_util_functions.py
+++ education/test/test_util_functions.py
@@ -9,35 +9,31 @@ from education.utils import get_week_count, get_months
 
 class TestUtilFunctions(TestCase):
     def test_should_return_week_count_between_two_dates(self):
-        today = datetime.today()
-        four_weeks_before = dateutils.increment(today,weeks=-4)
-        self.assertEqual(5,get_week_count(four_weeks_before,today))
+        start_date = datetime(2012,1,1)
+        four_weeks_before = dateutils.increment(start_date, weeks=-4)
+        self.assertEqual(5, get_week_count(four_weeks_before, start_date))
 
     def test_should_return_week_count_between_two_dates_passed_in_any_order(self):
-        today = datetime.today()
-        four_weeks_before = dateutils.increment(today,weeks=-4)
-        self.assertEqual(5,get_week_count(today,four_weeks_before))
+        start_date = datetime(2012, 1, 1)
+        four_weeks_before = dateutils.increment(start_date, weeks=-4)
+        self.assertEqual(5, get_week_count(start_date, four_weeks_before))
 
     def test_should_give_proper_month_data_starting_from_today(self):
-        today_date=datetime.today()
-        end_date = dateutils.increment(today_date,weeks=10)
+        start_date = datetime(2012, 1, 1)
+        end_date = dateutils.increment(start_date, weeks=10)
 
-        months = get_months(today_date,end_date)
-        self.assertEqual(months[0][0].date(),today_date.date())
-        self.assertEqual(months[-1][1].date(),end_date.date())
+        months = get_months(start_date, end_date)
+        self.assertEqual(months[0][0].date(), start_date.date())
+        self.assertEqual(months[-1][1].date(), end_date.date())
 
     def test_should_return_true_if_given_date_is_holiday(self):
-        today = datetime.today()
-        two_weeks_later = dateutils.increment(today,weeks=2)
-        three_weeks_later = dateutils.increment(today, weeks=3)
-        settings.SCHOOL_HOLIDAYS = [(today,two_weeks_later),(three_weeks_later,'1d')]
-        self.assertTrue(is_holiday(three_weeks_later,getattr(settings,'SCHOOL_HOLIDAYS')))
-
+        holiday_date = datetime(2012, 1, 1)
+        settings.SCHOOL_HOLIDAYS = [(holiday_date, '1d')]
+        self.assertTrue(is_holiday(holiday_date,getattr(settings,'SCHOOL_HOLIDAYS')))
 
     def test_should_return_false_if_given_date_is_not_holiday(self):
-        today = datetime.today()
-        two_weeks_later = dateutils.increment(today,weeks=2)
-        three_weeks_later = dateutils.increment(today, weeks=3)
-        four_weeks_later = dateutils.increment(today, weeks=4)
-        settings.SCHOOL_HOLIDAYS = [(today,two_weeks_later),(three_weeks_later,'1d')]
-        self.assertFalse(is_holiday(four_weeks_later,getattr(settings,'SCHOOL_HOLIDAYS')))
+        holiday_date = datetime(2012, 1, 1)
+        not_holiday_date = datetime(2012, 2, 1)
+
+        settings.SCHOOL_HOLIDAYS = [(holiday_date,'1d')]
+        self.assertFalse(is_holiday(not_holiday_date,getattr(settings,'SCHOOL_HOLIDAYS')))
