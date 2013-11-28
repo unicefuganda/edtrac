2625965737fa8644566ab4b52c297cba57197937
Martin Atukunda <matlads@gmail.com>
2013-11-28 15:15:30 +0300

    more updates to uwsgi test.ini

--- edtrac_project/wsgi_app.py
+++ edtrac_project/wsgi_app.py
@@ -4,8 +4,6 @@ import sys, os
 filedir = os.path.dirname(__file__)
 sys.path.append(os.path.join(filedir))
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-
 from django.core.handlers.wsgi import WSGIHandler
 
 application = WSGIHandler()
