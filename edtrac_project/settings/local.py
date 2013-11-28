d607736104a5c177eddf58f4d0e1b29ca43477b5
Martin Atukunda <matlads@gmail.com>
2013-11-28 16:09:39 +0300

    Add django_debug_toolbar==0.9.4 to the local setups

--- edtrac_project/settings/local.py
+++ edtrac_project/settings/local.py
@@ -15,6 +15,19 @@ DATABASES = {
     }
 }
 
+INSTALLED_APPS += (
+   'debug_toolbar',
+)
+
+MIDDLEWARE_CLASSES += (
+   'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+
+INTERNAL_IPS += ('127.0.0.1', '::1')
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False
+}
 
 
 FIRST_TERM_BEGINS = datetime.datetime(2013, 2, 4)
