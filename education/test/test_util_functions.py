1d9a7de9b3d3c8c00964d19a5edf1c7c78cd44dc
Chris Ford <christophertford@gmail.com>
2014-01-20 11:14:57 +0300

    No need to touch the settings file in order to unit test a method that doesn't read from it.

--- education/test/test_util_functions.py
+++ education/test/test_util_functions.py
@@ -8,6 +8,7 @@ from education.utils import get_week_count, get_months, _next_thursday
 
 
 class TestUtilFunctions(TestCase):
+
     def test_should_return_week_count_between_two_dates(self):
         start_date = datetime(2012,1,1)
         four_weeks_before = dateutils.increment(start_date, weeks=-4)
@@ -28,8 +29,7 @@ class TestUtilFunctions(TestCase):
 
     def test_should_return_true_if_given_date_is_holiday(self):
         holiday_date = datetime(2012, 1, 1)
-        settings.SCHOOL_HOLIDAYS = [(holiday_date, '1d')]
-        self.assertTrue(is_holiday(holiday_date,getattr(settings,'SCHOOL_HOLIDAYS')))
+        self.assertTrue(is_holiday(holiday_date, [(holiday_date, '1d')]))
 
     def test_should_return_true_if_given_date_is_in_holidays(self):
         holidays = [(datetime(2012, 1, 1), datetime(2012, 1, 3))]
@@ -42,9 +42,7 @@ class TestUtilFunctions(TestCase):
     def test_should_return_false_if_given_date_is_not_holiday(self):
         holiday_date = datetime(2012, 1, 1)
         not_holiday_date = datetime(2012, 2, 1)
-
-        settings.SCHOOL_HOLIDAYS = [(holiday_date,'1d')]
-        self.assertFalse(is_holiday(not_holiday_date,getattr(settings,'SCHOOL_HOLIDAYS')))
+        self.assertFalse(is_holiday(not_holiday_date, [(holiday_date, '1d')]))
 
     def test_should_determine_next_thursday(self):
         monday = date(2013, 8, 26)
