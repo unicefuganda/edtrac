ac8fde8798848fe6634857d7aae0f3781d474b17
Chris Ford <christophertford@gmail.com>
2014-02-13 13:34:48 +0000

    [Emily, Chris] Add docstring.

--- education/send.py
+++ education/send.py
@@ -3,6 +3,9 @@ from rapidsms.models import Connection
 from unregister.models import Blacklist
 
 def broadcast(text):
+    """
+    Schedule a one-off message for every non-blacklisted connection.
+    """
     for connection in Connection.objects.all(): 
         if not Blacklist.objects.filter(connection = connection).exists():
             Message.objects.create(connection = connection, text = text, direction='O')
