dd6ff5f2706bdc4a8e1c19ba9b31151c65baf242
Chris Ford <christophertford@gmail.com>
2014-02-13 13:54:22 +0000

    Fix broadcast command.

--- education/management/commands/broadcast.py
+++ education/management/commands/broadcast.py
@@ -1,20 +1,14 @@
 from django.core.management.base import BaseCommand
 from optparse import make_option
-from rapidsms_httprouter.router import get_router
-from rapidsms.models import Connection
+from education.send import broadcast
 
 class Command(BaseCommand):
 
     option_list = BaseCommand.option_list + (
-        make_option("-p", "--phone", dest="phone"),
         make_option("-t", "--text", dest="text"),
     )
 
     def handle(self, **options):
-	phone = options['phone'] or raw_input('Phone number you wish the message to appear to come from: ')
         text = options['text'] or raw_input('Text of the message: ')
-
-	connection  = Connection.objects.get(identity = phone)
-        router = get_router()
-        handled = router.handle_incoming(connection.backend.name, connection.identity, text)
+        broadcast(text)
         self.stdout.write('Done!\n')
