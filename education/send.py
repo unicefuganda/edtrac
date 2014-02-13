7c7802854723384dbedd44afa84f9464d8d3021c
Chris Ford <christophertford@gmail.com>
2014-02-13 15:48:37 +0000

    [Emily, Chris] Use a standard API method rather than rolling our own to decrease the chances of incompatibility.

--- education/send.py
+++ education/send.py
@@ -6,6 +6,5 @@ def broadcast(text):
     """
     Schedule a one-off message for every non-blacklisted connection.
     """
-    for connection in Connection.objects.all(): 
-        if not Blacklist.objects.filter(connection = connection).exists():
-            Message.objects.create(connection = connection, text = text, direction='O', status='Q')
+    connections = [c for c in Connection.objects.all() if not Blacklist.objects.filter(connection=c).exists()]
+    Message.mass_text(text, connections)
