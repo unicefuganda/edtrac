3a71b3d4949075c6c7014c4a7b8fc4672cdc6f61
Kenneth Matovu <matovken@gmail.com>
2014-05-19 10:34:56 +0300

    exclude quit reporters from excelsheet

--- edtrac_project/rapidsms_edtrac/education/management/commands/export_reporters.py
+++ edtrac_project/rapidsms_edtrac/education/management/commands/export_reporters.py
@@ -6,6 +6,7 @@ Created on Aug 27, 2012
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from unregister.models import Blacklist
 from education.models import EmisReporter
 import xlwt
 from rapidsms.contrib.locations.models import Location
@@ -15,12 +16,14 @@ from rapidsms.models import Connection
 class Command(BaseCommand):
     """Generates Reports By District"""
 
-#    def reporting_districts(self):
+    #    def reporting_districts(self):
 
     def handle(self, **options):
         """ Reporting Districts """
+
         def reporting_districts():
-            locations = Location.objects.filter(name__in=EmisReporter.objects.values_list('reporting_location__name',flat=True).distinct())
+            locations = Location.objects.filter(
+                name__in=EmisReporter.objects.values_list('reporting_location__name', flat=True).distinct())
             locs_list = []
             for loc in locations:
                 if not Location.tree.root_nodes()[0].pk == loc.pk and loc.type.name == 'district':
@@ -36,12 +39,14 @@ class Command(BaseCommand):
             for colx, value in enumerate(headings):
                 sheet.write(rowx, colx, value)
             sheet.set_panes_frozen(True)
-            sheet.set_horz_split_pos(rowx+1)
+            sheet.set_horz_split_pos(rowx + 1)
             sheet.set_remove_splits(True)
             rowx = 1
 
-            for rep in EmisReporter.objects.filter(reporting_location__in=district.get_descendants(include_self=True)):
-#                data = [district.name, rep.schools.all(), rep.name, Connection.objects.filter(contact=rep)]
+            for rep in EmisReporter.objects.filter(
+                    reporting_location__in=district.get_descendants(include_self=True)).exclude(
+                    pk__in=Blacklist.objects.values_list('connection__contact', flat=True)):
+                #                data = [district.name, rep.schools.all(), rep.name, Connection.objects.filter(contact=rep)]
                 sheet.write(rowx, 0, district.name)
                 sheet.write(rowx, 1, rep.name)
                 grps = ','.join(rep.groups.all().values_list('name', flat=True))
@@ -51,7 +56,7 @@ class Command(BaseCommand):
                 sheet.write(rowx, 4, connections)
                 names = ','.join(rep.schools.all().values_list('name', flat=True))
                 sheet.write(rowx, 5, names)
-                rowx +=1
+                rowx += 1
 
         file_name = "reporters.xls"
         file_path = '%s%s' % (settings.SPREADSHEETS_PATH, file_name)
